5591d17e3a80f6bb61ca07de04571feeaaf6ada1
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-06-21 20:57:31 -0500

    Creación de seeder y migración de caración de tabla genres.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder {
 
 		$this->call('AuditoriumsTableSeeder');
 
+		$this->call('GenresTableSeeder');
+
 	}
 
 }
---
+++ app/database/seeds/GenresTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+class GenresTableSeeder extends Seeder {
+
+    public function run()
+    {
+    	$genres = array();
+
+    	$names = App::make('names');
+
+    	$namesLen = count($names) - 1;
+
+        for( $i = 0; $i < 6; $i++)
+        {
+            $genre = array(
+                'name' => $names[$i],
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+                );
+
+            if( rand(0,1) == 1 && $i > 1)
+            {
+                array_add($genre,'venue',1);
+            }
+
+            array_push($genres, $genre);
+        }
+
+
+        DB::table('genres')->truncate();
+
+        DB::table('genres')->insert($genres);
+    }
+
+}
