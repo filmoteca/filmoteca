c6fb1a1f014ab21e8e4d08e17fba5b8d741284b1
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-06-25 20:56:49 -0500

    Se agregarón macros para generar varias etiquetas style y script a la vez.

---
+++ app/html.macros.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Recibe un array de links hacia scripts.
+ * Cada entrada puede ser o una url hacia el script
+ * o un array cuya primer entrada es un link y cuya
+ * segunda entrada es una lista de atributos.
+ * @var array
+ */
+HTML::macro('scripts', function($scripts = array()){
+
+	return HTML::resource('script', $scripts);
+
+});
+
+HTML::macro('styles', function($styles = array())
+{
+	return HTML::resource('style', $styles);
+});
+
+
+HTML::macro('resource', function($type, $list = array())
+{
+	if( $type != 'script' && $type != 'style') return '';
+
+	$html = '';
+
+	foreach($list as $element)
+	{
+		if( is_string($element))
+		{
+			$html .= HTML::{$type}( $element );
+		}else{
+		
+			if (!isset($element[1]))
+			{
+				$element[1] = array();
+			}
+
+			$html .= HTML::{$type}( $element[0], $element[1]);
+		}
+	}
+
+	return $html;
+});
