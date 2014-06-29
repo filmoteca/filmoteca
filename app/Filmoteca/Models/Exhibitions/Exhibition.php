2eabf8d72f82e46575d379639f74cec09027a404
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-06-28 22:44:16 -0500

    Se agregó un el metodo getTechnicalCard para obtener únicamente los lo datos tecnicos de la película.

--- app/Filmoteca/Models/Exhibitions/Exhibition.php
+++ app/Filmoteca/Models/Exhibitions/Exhibition.php
@@ -2,6 +2,8 @@
 
 use Eloquent;
 
+use Filmoteca\Genre;
+
 class Exhibition extends Eloquent
 {
 	public function exhibitionFilm()
@@ -22,5 +24,53 @@ class Exhibition extends Eloquent
 	public function auditoriums()
 	{
 		return $this->hasManyThrough('Filmoteca\Models\Exhibitions\Auditorium', 'Schedule');
+	}
+
+	public function getTechnicalCard()
+	{
+		$genresList = Genre::all(array('id','name'))
+			->lists('name','id');
+
+		$tc = array(); //tecnicalCard.
+
+		$film = $this->exhibition_film->film;
+
+		$tc['título'] 	= $film->title;
+
+		$tc['año'] 		= $film->year;
+
+		$tc['pais'] 	= $film->country_id;
+
+		$tc['duración'] = $film->duration;
+
+		$tc['género']	= $genresList[$film->genre_id];
+
+		$tc['director']	= $film->director;
+
+		$tc['guión']	= $film->script;
+
+		$tc['fotografía'] = $film->photographic;
+
+		$tc['música'] 	= $film->music;
+
+		$tc['edición']	= $film->edition;
+
+		$tc['producción'] = $film->production;
+
+		$tc['reparto']	= $film->cast;
+
+		$tc['sinopsis']	= $film->synopsis;
+
+		$technicalCard = array();
+
+		foreach( $tc as $key => $value)
+		{
+			if( !empty($value) )
+			{
+				$technicalCard[$key] = $value;
+			}
+		}
+
+		return $technicalCard;
 	}
 }
