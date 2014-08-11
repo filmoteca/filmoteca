d1e2e8d6a249c4bee06c2e897b860dca9d0eb206
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-08-10 21:10:06 -0500

    Se agregó un archivo de traducción de fechas. Incluye sólo los mese.

---
+++ app/lang/es/dates.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'months' => array(
+		'January' 	=> 'enero',
+		'February' 	=> 'febrero',
+		'March' 	=> 'marzo',
+		'April' 	=> 'abril',
+		'May'		=> 'mayo',
+		'June'		=> 'junio',
+		'July'		=> 'julio',
+		'August'	=> 'agosto',
+		'September' => 'septiembre',
+		'October'	=> 'octubre',
+		'Novembre'	=> 'noviembre',
+		'December' 	=> 'diciembre'));
