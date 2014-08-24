807587ed7c24cc6b8ebcdc2d57a1505843db2950
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-08-24 13:40:56 -0500

    Se mejoro la directiva que filtra.
    Ahora emite un evento con datos del filtro aplicado.

--- public/apps/directives/FilmotecaFilters.js
+++ public/apps/directives/FilmotecaFilters.js
@@ -2,6 +2,8 @@
  * @author Victor Aguilar
  */
 
+/* globals define, jQuery, angular*/
+
 (function(factory)
 {
 	'use strict';
@@ -15,7 +17,7 @@
 			],
 			factory);
 	}else{
-		factory($,angular);
+		factory(jQuery,angular);
 	}
 })(function($,angular)
 {
@@ -35,52 +37,71 @@
 		}
 	});
 
-	var flmFilters = ['flmFiltersConfig',function (config) {		
-		return {
-			restrict: 'A',
-			scope: {
-				filter :'@filter',
-				value : '@value',
-			},
-			link: function($scope, element) 
-			{
+	var flmFilters = ['flmFiltersConfig',function (config) 
+	{
+		var link = function($scope, element) 
+		{
+			element.on('click', function()
+			{	
 				var selectedItems = [];
 
 				var unselectedItems = [];
 
-				element.on('click', function()
-				{	
-					/**
-					 * Angular (o firefox) le pone comillas dobles a value, aunque ya 
-					 * tenga comillas dobles.
-					 */
-					$scope.value = $scope.value.replace('"','','gim');
+				/**
+				 * Angular (o firefox) le pone comillas dobles a value, aunque ya 
+				 * tenga comillas dobles.
+				 */
+				$scope.value = $scope.value.replace('"','','gim');
 
-					var filter = config.filters.default;
+				var filter = config.filters.default;
 
-					if( typeof config.filters[$scope.filter] === 'function' ) 
+				if( typeof config.filters[$scope.filter] === 'function' ) 
+				{
+					filter = config.filters[$scope.filter];
+				}
+
+				var items = config.items;
+
+				angular.forEach(items, function(item)
+				{
+					if( filter(item, $scope.value) )
 					{
-						filter = config.filters[$scope.filter];
+						selectedItems.push( '#' + item.id );
+					}else
+					{
+						unselectedItems.push( '#' + item.id );
 					}
-
-					var items = config.items;
-
-					angular.forEach(items, function(item)
-					{
-						if( filter(item, $scope.value) )
-						{
-							selectedItems.push( '#' + item.id );
-						}else
-						{
-							unselectedItems.push( '#' + item.id );
-						}
-					});
-
-					angular.element( selectedItems.join(',') ).show();
-					
-					angular.element( unselectedItems.join(',') ).hide();
 				});
-			}
+
+				angular.element( selectedItems.join(',') ).show();
+				
+				angular.element( unselectedItems.join(',') ).hide();
+
+				var title = (angular.isDefined($scope.title))? 
+											$scope.title : $scope.filterValue;
+
+				var data = {
+					name 	: $scope.filter,
+					value 	: $scope.value,
+					title 	: title,
+					results : selectedItems.length
+				};
+
+				$scope.$root.safeApply( function()
+				{
+					$scope.$emit('filterSelected', data );
+				});
+			});
+		};
+		
+		return {
+			restrict: 'A',
+			scope: {
+				filter 	: '@filterName',
+				value 	: '@filterValue',
+				title 	: '@filterTitle'
+			},
+			link: link
 		};
 	}];
 
