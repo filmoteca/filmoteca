620db4d8e5d36b4fd99c26e86ba86fa6115ad06c
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-09-16 20:20:10 -0500

    Se cro ResourceRepository y se movio el codigo de FilmsRepository a esta última.

--- app/Filmoteca/Repository/FilmsRepository.php
+++ app/Filmoteca/Repository/FilmsRepository.php
@@ -2,42 +2,11 @@
 
 use Filmoteca\Models\Film;
 
-class FilmsRepository
+class FilmsRepository extends ResourcesRepository
 {
 	public function __construct(Film $film)
 	{
-		$this->film = $film;
-	}
-
-	public function store(array $data = null)
-	{
-		if( empty( $data ) ) throw new Excpetion('Empty data');
-
-		return $this->film->create($data);
-	}
-
-	public function find($id)
-	{
-		return $this->film->findOrFail($id);
-	}
-
-	public function paginate($amount)
-	{
-		return $this->film->paginate($amount);
-	}
-
-	public function update($id,array $data = null)
-	{
-		if( empty( $data ) ) throw new Excpetion('Empty data');
-
-		return $this->film->findOrFail($id)->fill($data)->save();
-	}
-
-	public function destroy($id)
-	{
-		$this->film->destroy($id);
-
-		return is_null( $this->film->find($id) );
+		$this->resource = $film;
 	}
 }
 
---
+++ app/Filmoteca/Repository/ResourcesRepository.php
@@ -0,0 +1,36 @@
+<?php namespace Filmoteca\Repository;
+
+class ResourcesRepository
+{
+	public function store(array $data = null)
+	{
+		if( empty( $data ) ) throw new Excpetion('Empty data');
+
+		return $this->resource->create($data);
+	}
+
+	public function find($id)
+	{
+		return $this->resource->findOrFail($id);
+	}
+
+	public function paginate($amount)
+	{
+		return $this->resource->paginate($amount);
+	}
+
+	public function update($id,array $data = null)
+	{
+		if( empty( $data ) ) throw new Excpetion('Empty data');
+
+		return $this->resource->findOrFail($id)->fill($data)->save();
+	}
+
+	public function destroy($id)
+	{
+		$this->resource->destroy($id);
+
+		return is_null( $this->resource->find($id) );
+	}
+}
+
