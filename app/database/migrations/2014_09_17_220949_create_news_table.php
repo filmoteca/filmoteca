f74f6520332d4249eff5a9fd14442cae32fb74f5
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-09-17 22:24:28 -0500

    #18 Creación de migraciones para las Noticias.

---
+++ app/database/migrations/2014_09_17_220949_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('news', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title', 255)->nullable(false);
+			$table->text('body')->nullable(false);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('news');
+	}
+
+}
