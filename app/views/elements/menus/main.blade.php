214fcfdafda0bc75ba40600b9fcc3acb205ef7a9
monyc2000 <monica.guzman.r@gmail.com>
2014-09-25 17:51:46 -0500

    Corrección de links y letreros del menú principal

--- app/views/elements/menus/main.blade.php
+++ app/views/elements/menus/main.blade.php
@@ -1,7 +1,7 @@
 <div class="main-menu">
 	<ul>
 		<li>
-			<a><span>Quienes somos</span></a>
+			<a><span>Quiénes somos</span></a>
 			<ul>
 				<li>
 					<a href="/pages/quienes-somos/mision-y-vision">
@@ -51,7 +51,7 @@
 			<a href="pages/acervos/index"><span>Acervo</span></a>
 	        <ul>
 				<li>
-					<a href="/pages/resguardo-y-conservacion">
+					<a href="/pages/acervo/resguardo-y-conservacion">
 						<span>Resguardo y conservación</span>
 					</a>
 				</li>
@@ -61,7 +61,7 @@
 					</a>
 				</li>
 				<li>
-					<a href="/pages/quines-somos/objetivos-y-funciones">
+					<a href="/pages/acervo/aparatos-antiguos">
 						<span>Acervo de aparatos antiguos</span>
 					</a>
 				</li>
@@ -72,7 +72,7 @@
 				</li>
 				<li>
 					<a href="http://www.filmoteca.unam.mx/cinelinea/">
-						<span>Objetivos y Funciones</span>
+						<span>Cine en línea</span>
 					</a>
 	             </li>                    
 	        </ul>
@@ -82,48 +82,53 @@
 	      	<a href="#"><span>Servicios</span></a>
 	        <ul>
 				<li>
-					<a href="/pages/lineamientos-generales-para-acceder-al-material">
+					<a href="/pages/servicios/lineamientos-generales-para-acceder-al-material">
 						<span>Lineamientos generales para acceso al material</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/banco-de-imagen">
+					<a href="/pages/servicios/banco-de-imagen">
 						<span>Banco de imagen</span>
 					</a>
 				</li>
 				<li>
-					<a href="'/pages/departamento-de-catalogacion'">
+					<a href="'/pages/servicios/departamento-de-catalogacion'">
 						<span>Catalogación</span>
 					</a>
 				</li>
 	            <li>
-					<a href="/pages/resguardo-y-conservacion">
-						<span>epósito y resguardo</span>
+					<a href="/pages/servicios/deposito-y-resguardo">
+						<span>Depósito y resguardo</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/museografia">
+					<a href="/pages/servicios/exposiciones-museografia">
 						<span>Exposiciones</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/laboratorio">
-						<span>Laboratorio</span>
+					<a href="/pages/servicios/laboratorio-fotoquimico">
+						<span>Laboratorio fotoquímico</span>
 					</a>
-	             </li>  
+	             </li> 
+	             <li>
+					<a href="/pages/servicios/laboratorio digital">
+						<span>Laboratorio digital</span>
+					</a>
+	             </li>   
 	            <li>
-					<a href="/pages/prestamo-y-alquiler-de-peliculas">
+					<a href="/pages/servicios/prestamo-y-alquiler-de-peliculas">
 						<span>Préstamo y alquiler de películas</span>
 					</a>                          
 	        		}}
 	            </li>
 	            <li>
-					<a href="/pages/produccion">
+					<a href="/pages/servicios/departamento-de-produccion">
 						<span>Producción</span>
 					</a>
 	            </li>
 	            <li>
-					<a href="/pages/taller-de-restauracion">
+					<a href="/pages/servicios/taller-de-restauracion">
 						<span>Taller de restauración</span>
 					</a>
 	           	</li>
@@ -131,28 +136,28 @@
 		</li>
 		
 		<li>
-			<a href="/pages/departamento-de-documentacion">
+			<a href="#">
 				<span>Centro de documentación</span>
 			</a>
 			<ul>
 				<li>
-					<a href="/pages/consulta-material">
+					<a href="/pages/documentacion/consulta-material">
 						<span>Consulta de material</span>
 					</a>
 				</li>
 	            <li>
-					<a href="/pages/libros digitales">
+					<a href="/pages/documentacion/libros digitales">
 						<span>Libros digitales</span>
 					</a>
 				</li>
 				
 				<li>
-					<a href="/pages/colecciones">
+					<a href="/pages/documentacion/colecciones">
 						<span>Colecciones</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/reglamento'">
+					<a href="/pages/documentacion/reglamento'">
 						<span>Reglamento</span>
 					</a>
 				</li>
@@ -188,23 +193,23 @@
 			</a>
 	        <ul>
 				<li>
-					<a href="/pages/noticias">
+					<a href="/pages/prensa/noticias">
 						<span>Noticias</span>
 					</a>
 
 				</li>
 				<li>
-					<a href="/pages/filmoteca-medios">
+					<a href="/pages/prensa/filmoteca-medios">
 						<span>Filmoteca en los medios'</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/galeria-de-eventos">
+					<a href="/pages/prensa/galeria-de-eventos">
 						<span>Galeria</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/entrevistas">
+					<a href="/pages/prensa/entrevistas">
 						<span>Entrevistas</span>
 					</a>
 				</li>
@@ -221,32 +226,37 @@
 
 		</li>
 		<li>
-			<a href="/pages/quines-somos/objetivos-y-funciones">
+			<a href="/pages/extension-academica/index">
 				<span>Extensión académica</span>
 			</a>
 			<ul>
 				<li>
-					<a href="/pages/quines-somos/objetivos-y-funciones">
+					<a href="/pages/extension-academica/procedimiento-de-inscripcion">
+						<span>Procedimiento de inscripción</span>
+					</a>
+				</li>
+				<li>
+					<a href="/pages/extension-academica/procedimiento-de">
 						<span>Cursos y talleres</span>
 					</a>
 				</li>
 				<li>
-					<a href="//pages/atencion-alumnos">
+					<a href="/pages/extension-academica/atencion-a-alumnos">
 						<span>Atención a alumnos</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/servicio-social">
+					<a href="/pages/extension-academica/servicio-social">
 						<span>Servicio social</span>
 					</a>
 				</li> 
 				<li>
-					<a href="pages/becarios">
+					<a href="/pages/extension-academica/becarios">
 						<span>Becarios</span>
 					</a>
 				</li> 
 				<li>
-					<a href="/pages/visitas-guiadas">
+					<a href="/pages/extension-academica/visitas-guiadas">
 						<span>Visitas guiadas</span>
 					</a>
 				</li> 
@@ -259,22 +269,22 @@
 			</a>
 			<ul>
 				<li>
-					<a href="/pages/alfonso-reyes">
-						<span>Fósforo</span>
+					<a href="/pages/concursos/alfonsoreyes">
+						<span>Alfonso Reyes "Fósforo"</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/rovirosa">
+					<a href="/pages/concursos/jose-rovirosa">
 						<span>José Rovirosa</span>
 					</a>
 				</li>
 	            <li>
-					<a href="/pages/corto-movil">
+					<a href="/pages/concursos/corto-movil">
 						<span>Corto Móvil</span>
 					</a>
 				</li> 
 				<li>
-					<a href="/pages/corto-movil">
+					<a href="/pages/concursos/convocatorias">
 						<span>Convocatorias</span>
 					</a>
 				</li> 
@@ -284,7 +294,7 @@
 
 		<li>
 			<a href="{{ route('shop')}}">
-				<span>Objetivos y Funciones</span>
+				<span>Librería y Tienda</span>
 			</a>
 		</li>
 	</ul>
