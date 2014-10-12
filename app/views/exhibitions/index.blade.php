45edf190e5f90374f6cd9c7a33821f2f763bb54d
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-10-11 20:40:49 -0500

    El index de exhibiciones ahora corresponde a administración.

--- app/views/exhibitions/index.blade.php
+++ app/views/exhibitions/index.blade.php
@@ -1,154 +1,60 @@
-@extends('layouts.default')
-
-@section('scripts')
-
-@section('scripts')
-
-	<script>
-		
-		window.exhibitions = {{ $exhibitions->toJson() }}
-
-	</script>
-
-	{{-- 
-		Este template sobre-escribe al template 
-		templates/datepicker/day.html de anguler-ui datepicker  
-	--}}
-	<script id="template/datepicker/day.html" type="text/ng-template">
-		@include('exhibitions.dayorweekpicker')
-	</script>
-
-	<script id="templates/exhibitions/list.html" type="text/ng-template">
-		@include('exhibitions.list', array('exhibitions', $exhibitions))
-	</script>
-	
-{{ HTML::script('/apps/require.config.js')}}
-
-{{ HTML::script(
-	'/bower_components/requirejs/require.js', 
-	['data-main' =>'/apps/pages/exhibition/App.js'])
-}}
-@stop
-
-@section('styles')
-	{{ HTML::styles(array(
-		'/bower_components/angucomplete-alt/angucomplete-alt.css',
-		'/assets/css/exhibitions-datepicker.css')) }}
-@stop
-
-@section('breadcrumbs')
-	<li class="active">
-		Programación
-	</li>
-@stop
-
 @section('content')
 
-	<div class="sidebar">
+<h2>Lista de Exhibiciones</h2>
 
-		<span class="glyphicon glyphicon-search pull-left"></span>
+{{ $resources->links() }}
 
-		<div class="search-autocomplete pull-right">
-			<angucomplete-alt id="ex1"
-				place-holder="Search countries"
-				pause="0"
-				selected-object="selectedAdvice"
-				local-data="advices"
-				title-field="title"
-				description-field="director"
-				search-fields="title,director"
-				image-field="thumbnail"
-				minlength="1"
-				text-searching="Buscando..."
-				text-no-results="Ninguna exhibición encontrada"
-				input-class="form-control form-control-small">
-			</angucomplete-alt>
-		</div>
-
-		<div class="exhibition-datepicker" style="display:inline-block; min-height:200px;">
-			<h3>Consultar Programación</h3>
-
-			<datepicker ng-model="dphone" class="well well-sm"></datepicker>
-		</div>
-
-		<div class="static-pages-menu">
-			<ul>
-				<li class="has-sub">
-					<a><span>Salas</span></a>
-					<ul>
-						<li class="last">
-							<a flm-filters filter-name="auditorium" filter-value="0"
-								class="btn">
-								<span>Cualquiera</span>
-							</a>
-						</li>
-						@foreach($auditoriums as $auditorium )
-							<li class="last">
-								<a flm-filters 
-									filter-name="auditorium" 
-									filter-value="{{ $auditorium->id }}"
-									filter-title="{{ $auditorium->name }}"
-									class="btn">
-									<span>{{$auditorium->name}}</span>
-								</a>
-							</li>
-						@endforeach
-					</ul>
-				</li>
-
-				<li class="has-sub">
-					<a><span>Funciones Especiales</span></a>
-					<ul>
-						<li class="last">
-							<a flm-filters filter-name="icon" filter-value="0" 
-								class="btn">
-								<span>Cualquiera</span>
-							</a>
-						</li>
-						@foreach($icons as $icon)
-							<li class="last">
-								<a flm-filters 
-									filter-name="icon" 
-									filter-value="{{ $icon->id }}"
-									filter-title="{{ $icon->name }}"
-									class="btn">
-									<span>
-										{{ HTML::image($icon->image->url('thumbnail'), $icon->name) }}
-									</span>
-								</a>
-							</li>
-						@endforeach
-					</ul>
-				</li>
-			</ul>
-		</div>
-
-		<div class="subscribe-box">
-			
-			<p>Recibe la cartelera digital directo a tu correo electrónico.</p>
-
-			<div class="input-group input-group-sm">
-				<input type="email" 
-					name="email" 
-					placeholder="Ingresa tu correo electrónico"
-					class="form-control">
-				<span class="input-group-addon">@</span>
-			</div>
-
-			<button type="button" class="btn btn-success">Registrarse</button>
-		</div>
-	</div>
-
-	<div class="content">
-		<div class="exhibition-list" ng-show="!urlToDetails">
-			@include('exhibitions.list', array('exhibitions', $exhibitions))
-		</div>
-
-		<div class="loading" ng-show="loading"></div>
-
-		<div class="exhibition-detail" data-ng-include="urlToDetails" ng-show="urlToDetails">
-
-		</div>
-	</div>
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Título de la película</th>
+			<th>Director</th>
+			<th>Horarios</th>
+			<th>Portada</th>
+			<th>Acciones</th>
+		</tr>
+	</thead>
 	
+	<tbody>
+		@foreach($resources as $exhibition )
+			<tr>
+				<td>{{ $exhibition->id }}</td>
+				<td>{{ $exhibition->exhibition_film->film->title }}</td>
+				<td>{{ $exhibition->exhibition_film->film->director }}</td>
+				
+				<td>
+				@foreach( $exhibition->auditoriums as $auditorium)
+					<div class="panel panel-default">
+						<div class="panel-heading">{{ $auditorium->name }}</div>
+						<div class="panel-body">
+							<ul class="list-group">						
+							@foreach( $exhibition->schedulesByAuditorium($auditorium->id) as $schedule)
+								<li class="list-group-item">{{ $schedule->entry }}</li>
+							@endforeach
+							</ul>
+						</div>
+					</div>
+				@endforeach
+				</td>
+
+				<td><img src="{{ $exhibition->exhibition_film->film->image->url('thumbnail') }}" alt="{{ $exhibition->exhibition_film->film->title }}" class="image thumbnail"></td>
+				<td>
+					{{ HTML::linkRoute('admin.exhibition.show', 'Ver',
+						array($exhibition->id),
+						array('class'=> 'btn btn-success')) }}
+					<br>
+					{{ HTML::linkRoute('admin.exhibition.edit', 'Editar',
+						array($exhibition->id),
+						array('class'=> 'btn btn-info')) }}
+					<br>
+					{{ Form::open(array('route' => array('admin.exhibition.destroy', $exhibition->id), 'method'=> 'DELETE')) }}
+						<button class="btn btn-danger" type="submit">Borrar</button>
+					{{ Form::close()}}
+				</td>
+			</tr>
+		@endforeach
+	</tbody>
+</table>
+
 @stop
