beb3886367c61d07cade0f7ecad7fee62079f3ed
monyc2000 <monica.guzman.r@gmail.com>
2014-10-22 18:04:23 -0500

    Se crearon estas nuevas páginas Pendiente el contenido

---
+++ app/views/pages/servicios/centro-de-documentacion.blade.php
@@ -0,0 +1,43 @@
+@section('breadcrumbs')
+<li>
+	<a href="/pages/servicios/index">
+		Servicios
+	</a>
+</li>
+<li class="active">
+	Centro de documentación
+</li>
+@stop
+
+
+
+
+
+@section('content')
+
+<div class="sidebar">
+	@include('elements.menus.servicios', array('selected' => 3))
+</div>
+
+<div class="content">
+	<div class="img-servicios">
+		<img src="/assets/imgs/fragmentos.jpg" aling="left">
+	</div>
+	<h1>Centro de documentación</h1>
+
+	<p>Esta sección apoya principalmente 
+	</p>
+
+	
+	<p>Responsable: Antonia Rojas</p> 
+	<p>Teléfono: 56 22 93 76 </p>
+	<p>Correo electrónico: <a href="mailto:antonia@unam.mx">carriles@unam.mx</a></p>
+
+	<p>*Para tener acceso a este departamentos consultar 
+	{{ HTML::link(
+		'/pages/servicios/lineamientos_generales_para_acceder_al_material',
+		'Lineamientos generales para acceso al material' )                            
+		}}</p>
+</div>
+
+@stop
