ad346ee8e5608bb5349a8fdc8d4d1ff140a01e8a
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-11-23 18:37:52 -0600

    Migraciones para relacionar un estudiante con un usuario del sistema de cursos.

---
+++ app/database/migrations/2014_11_23_133710_add_foreign_key_in_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyInStudentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('students', function(Blueprint $table)
+		{
+			$table->foreign('user_id')
+					->references('id')
+					->on('users')
+					->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('students', function(Blueprint $table)
+		{
+			$table->dropColumn('user_id');
+		});
+	}
+
+}
