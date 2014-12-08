fcbab65053e8c0f16228c1d6622cc27ba6245298
monyc2000 <monica.guzman.r@gmail.com>
2014-12-07 23:46:32 -0600

    Se corrigió el nombre de las clases y ancho de las columnas. Esta prueba para incluir pdf -no abre

--- app/views/pages/quienes-somos/consejo-asesor.blade.php
+++ app/views/pages/quienes-somos/consejo-asesor.blade.php
@@ -27,13 +27,12 @@
 	<p>La integración de cuerpos colegiados universitarios, tiene como objetivo fortalecer y articular la planeación de tareas, programas y actividades de la dependencia y coadyuvar a la articulación y cumplimiento de sus funciones sustantivas, además de evaluar, colaborar y aportar ideas para difundir con la mayor amplitud posible los valores culturales, en particular los artísticos, a fin de contribuir a la formación integral de los universitarios.</p>
 
 	<p>El Consejo Asesor de la Dirección General de Actividades Cinematográficas está integrado actualmente de la siguiente manera:</p>
-				
 
-	<div class="consejeros">
-		<div class="img_consejero">
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/assets/imgs/documento-pdf.png">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">María Guadalupe Ferrer Andrade</span>
 			<br>
 			<span class="cargo">Directora General y Presidente del Consejo
@@ -42,88 +41,102 @@
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/marycarmen_de_lara.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Cineasta María del Carmen De Lara</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
-			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
+			<img src="/assets/imgs/documento-pdf.png">
+			<a href="/public/pdf/Maria-del-Carmen-de-Lara.pdf">Resumen curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/carlos_martinez_assad.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Dr. Carlos Martínez Assad</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
+			<img src="/assets/imgs/documento-pdf.png">
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/fernando_macotela.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Lic. Fernando Macotela</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
+			<img src="/assets/imgs/documento-pdf.png">
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/armando_casas.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Cineasta Armando Casas</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
+			<img src="/assets/imgs/documento-pdf.png">
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/felipe_coria.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Cineasta Felipe Coria</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
+			<img src="/assets/imgs/documento-pdf.png">
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/rafael_avina.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Lic. Rafael Aviña</span>
 			<br>
 			<span class="cargo">Consejero
 			</span>
 			<br>
+			<img src="/assets/imgs/documento-pdf.png">
 			<a href="http://www.planeacion.unam.mx/Memoria/2009/PDF/8.2-DGAC.pdf" target="_blank">Resumen Curricular</a>
 		</div>
-	</div><div class="consejeros">
-		<div class="img_consejero">
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/nadina_illescas.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Mtra. Nadina Illescas Villegas</span>
 			<br>
 			<span class="cargo">Enlace Interinstitucional y Secretaria del Consejo
@@ -131,11 +144,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/carmen_carrara.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Mtra. Carmen Carrara y García</span>
 			<br>
 			<span class="cargo">Subdirectora de Difusión
@@ -143,11 +157,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/albino_alvarez.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Cineasta Albino Álvarez</span>
 			<br>
 			<span class="cargo">Subdirector de Rescate y Restauración
@@ -155,11 +170,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/miguel_angel_recillas.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Lic. Miguel Ángel Recillas</span>
 			<br>
 			<span class="cargo">Subdirección de Acervos
@@ -167,11 +183,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/imgs/consejeros/jose_manuel_garcia.jpg">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">José Manuel García</span>
 			<br>
 			<span class="cargo">Jefe de Acceso Interinstitucional
@@ -179,11 +196,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/assets/imgs/documento-pdf.png">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">Ximena Perujo Cano</span>
 			<br>
 			<span class="cargo">Jefe de Programación
@@ -191,11 +209,12 @@
 			<br>
 		</div>
 	</div>
-	<div class="consejeros">
-		<div class="img_consejero">
+	<br>
+	<div class="row">
+		<div class="col-sm-2">
 			<img src="/assets/imgs/documento-pdf.png">
 		</div>
-		<div class="consejero">
+		<div class="col-sm-5">
 			<span class="nombre">M.C. Gerardo León Lastra</span>
 			<br>
 			<span class="cargo">Coordinador de Nuevas Tecnologías
@@ -206,3 +225,6 @@
 </div>
 
 @stop
+
+
+			
