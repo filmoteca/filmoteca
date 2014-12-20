a3482b80dd3fc58b62393420876d4e333f9b7f76
Fabián Juárez <fabian@dommo.mx>
2014-12-19 22:35:28 -0600

    migration for competitions table

---
+++ app/database/migrations/2014_12_15_003712_create_competitions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCompetitionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('competitions', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->text('call_text');
+			$table->date('year');
+			$table->string('status', 45);
+			$table->date('start');
+			$table->date('end');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('competitions');
+	}
+
+}
