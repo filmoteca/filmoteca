8516773e252c00c646ddc4def93c6c21715a813d
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-01-06 19:43:13 -0600

    Se hicieron los widgets de facebbok y twitter respondimos.

--- app/views/pages/home/partials/social-networks.blade.php
+++ app/views/pages/home/partials/social-networks.blade.php
@@ -10,8 +10,9 @@
 							<span class="icon icon-facebook"></span>
 							<h3 class="h3">Facebook</h3>
 						</div>
-
-						<div class="fb-like-box" data-href="https://www.facebook.com/FilmotecaUNAM?ref=ts&amp;fref=ts" data-width="262" data-height="300" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+						<div class="fb-wrap">
+							<div class="fb-like-box" data-href="https://www.facebook.com/FilmotecaUNAM?ref=ts&amp;fref=ts" data-width="262" data-height="300" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -21,8 +22,8 @@
 						<div class="header"><span class="icon icon-twitter"></span><h3>Twitter</h3></div>
 
 						<div>
-						<a class="twitter-timeline" data-dnt="true" width="262" height="300" href="https://twitter.com/ButacaUNAM"  data-widget-id="444330226755977216">Tweets por @ButacaUNAM</a>
-						</div>	
+						<a class="twitter-timeline" data-dnt="true" width="100%" height="300" href="https://twitter.com/ButacaUNAM"  data-widget-id="444330226755977216">Tweets por @ButacaUNAM</a>
+						</div>
 					</div>
 				</div>
 			</div>
