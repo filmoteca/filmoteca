58f46f2aed3c70c688f94fa93c98ad4ba3de3491
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-01-17 14:24:59 -0600

    Fixed styles to news.

--- app/views/pages/home/partials/other-news.blade.php
+++ app/views/pages/home/partials/other-news.blade.php
@@ -6,7 +6,9 @@
 
 		<ul class="list-group">
 			@foreach( $news as $new )
-				<li class="list-group-item">
+				<li class="list-group-item preview-news">
+				    <img src="{{ asset($new->image->url('thumbnail')) }}">
+
 					<span class="preview-news">{{ Str::limit($new->body, 80, '') }}</span> <span class="see-more">{{ HTML::linkAction('NewsController@show', 'Leer más', [$new->id]) }}</span>
 				</li>
 			@endforeach
