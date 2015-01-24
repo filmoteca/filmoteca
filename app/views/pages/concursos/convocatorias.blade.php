1a38ea7ba1ed0a2e87795a280bf69be7b39a025f
monyc2000 <monica.guzman.r@gmail.com>
2015-01-24 00:51:33 -0600

    Se creó la carpeta para los archivos de Concursos

---
+++ app/views/pages/concursos/convocatorias.blade.php
@@ -0,0 +1,28 @@
+@section('breadcrumbs')
+	<li>
+		<a href="/pages/concursos/index">
+			Concursos
+		</a>
+	</li>
+	<li class="active">
+		Convocatorias
+	</li>
+@stop
+
+@section('sidebar')
+	@include('elements.menus.concursos', array('selected' => 3))
+@stop
+
+@section('content')
+
+<h1>Concurso Corto Móvil 2014</h1>
+
+	<p>PREMIO AL MEJOR CORTOMETRAJE REALIZADO CON DISPOSITIVOS MÓVILES</p>
+	<p>Consulta aquí las bases y ¡participa!</p>
+	<img src="/imgs/convocatorias/convocatoriacortomovil2014.jpg" aling="left">
+		
+		<p></p>
+		<p>Descarga el <a href="http://filmoteca.dev/pdf/convocatorias/registro14-corto-movil.pdf" target="_blank">Formulario de inscripción</a>
+		y el <a href="http://www.filmoteca.dev/pdf/convocatorias/autorizacion-corto-movil.pdf" target="_blank">Documento de conformidad.</a></p>
+
+@stop
