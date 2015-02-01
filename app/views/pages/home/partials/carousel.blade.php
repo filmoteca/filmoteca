4ccd787bda09e75beb0a16d7cb23e7daa907dfdf
monyc2000 <monica.guzman.r@gmail.com>
2015-01-31 21:14:51 -0600

    Se agregaron "id" a los textos del slider, para darle estilo. Se agregaron links y rutas a imágenes

--- app/views/pages/home/partials/carousel.blade.php
+++ app/views/pages/home/partials/carousel.blade.php
@@ -1,21 +1,54 @@
 <div class="flm-section carousel">
-	<div class="content">
-		<div class="main-carousel">
-			<div>
-				<div class="caption">
-					<p>Este fue el campeonato del año paso de downhill.</p>
-					<p> Donde, desafortunadamente,
-						Gee Atherton no puedo ganar.</p></div>
-				<img src="/imgs/BancodeIm.jpg" class="img-responsive"></div>
-			<div>
-				<div class="caption">Ella es jill valentine una miembro de STARS</div>
-				<img src="/imgs/BancodeIm.jpg" class="img-responsive">
-			</div>
-			<div>
-				<div class="caption">Dead Nation: doom</div>
-				<img src="/imgs/BancodeIm.jpg" class="img-responsive">
-			</div>
-		</div>
-	</div>
-	
+    <div class="content">
+        <div class="main-carousel">
+            <div>
+                <div class="caption">
+                    <div id="title">BORIS GOLDENBLANK
+                    </div>
+                    <br>
+                    <div id="text">La Filmoteca de la UNAM lamenta profundamente el fallecimiento de BORIS GOLDENBLANK, una de las figuras más destacadas en el ámbito cinematográfico de jalisco, quien será recordado no solamente por su extensa obra fílmica, sino por su carrera como docente.
+                    </div>   
+                </div>
+                <img src="/imgs/home/slider-home/Boris-Goldenblank.jpg" class="img-responsive">
+            </div>
+            <div>
+                <div class="caption">
+                    <div id="title">AMBULANTE
+                    </div>
+                    <div id="text">GIRA DE DOCUMENTALES 2015
+                        <br><p>Del 30 de febrero al 5 de marzo</p>
+                        <a href="{{ URL::to('exhibition') }}">Consulta la programación aquí</a>
+                    </div>
+                </div>
+                <a href="{{ URL::to('exhibition') }}">
+                <img src="/imgs/home/slider-home/ambulante-gira-de-documentales.jpg" class="img-responsive">
+            	</a>
+            </div>
+            <div>
+                <div class="caption">
+                    <div id="title">CORTO MÓVIL
+                    </div>
+                    <div id="text">CERRAMOS INSCRIPCIONES. Tienes hasta el 5 de febrero!!
+                    </div>  
+                </div>
+                <a href="/pages/concursos/convocatoria-corto-movil">
+                <img src="/imgs/home/slider-home/corto-movil.jpg" class="img-responsive">
+            	</a>
+            </div>
+            <div>
+                <div class="caption">
+                    <div id="title">CINE INDEPENDIENTE
+                    </div>
+                    <div id="text">No te pierdas esta Muestra.
+                    </div>
+                    <a href="{{ URL::to('exhibition') }}">Consulta la programación aquí</a>   
+                </div>
+                <a href="{{ URL::to('exhibition') }}">
+                <img src="/imgs/home/slider-home/cine-independiente.jpg" class="img-responsive">
+                </a>   
+            </div>
+
+        </div>
+    </div>
+   
 </div>
