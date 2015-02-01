de1abef0a45aee5c03a4a6f80c2a2bf23656c24a
monyc2000 <monica.guzman.r@gmail.com>
2015-01-31 23:18:54 -0600

    Se agregaron rutas y videos, se cambió el título. Se modificó para que únicamente se vean videos en esta sección.

--- app/views/pages/home/partials/the-last.blade.php
+++ app/views/pages/home/partials/the-last.blade.php
@@ -2,34 +2,41 @@
 	<div class="content">
 
 		<div class="header">
-			<h4>Lo último</h4>
+			<h4>NUESTRAS ACTIVIDADES</h4>
 			<span class="small">Rescatar, restaurar, conservar</span>
 		</div>
 
 		<div role="tabpanel">
 
 			<ul class="nav nav-tabs" role="tablist">
-				<li role="presentation" class="active"><a href="#videos" aria-controls="videos" role="tab" data-toggle="tab">Videos</a></li>
-				<li role="presentation"><a href="#reseñas" aria-controls="reseñas" role="tab" data-toggle="tab">Reseñas</a></li>
+				<li role="presentation" class="active"><a href="#video1" aria-controls="videos" role="tab" data-toggle="tab">Video 1</a></li>
+				<li role="presentation"><a href="#video2" aria-controls="videos" role="tab" data-toggle="tab">Video 2</a></li>
 			</ul>
 
 			<div class="tab-content">
-				<div role="tabpanel" class="tab-pane active" id="videos">
+				<div role="tabpanel" class="tab-pane active" id="video1">
 					<ul class="list-group">
 
-						<li class="list-group-item">Lorem ipsum Non aute Duis quis do Ut quis consectetur est ad. <span class="see-more"><a href="#">Ver video</a></span></li>
-
-						<li class="list-group-item">Lorem ipsum Non aute Duis quis do Ut quis consectetur est ad. <span class="see-more"><a href="#">Ver video</a></span></li>
+						<li class="list-group-item">La Filmoteca de la UNAM cumple 55 años. Estamos orgullosos de ser el acervo fílmico más grande de Latinoamérica...<span class="see-more"><a href="#">Leer más</a></span></li>
 
 						<div class="video-preview">
 							<div class="embed-responsive embed-responsive-4by3">
-									
+								<iframe width="420" height="315" src="https://www.youtube-nocookie.com/embed/hZhUqErqGCQ" frameborder="0" allowfullscreen></iframe>	
 							</div>
 						</div>
 					</ul>
 				</div>
-				<div role="tabpanel" class="tab-pane" id="reseñas">
-					Reseñas
+				<div role="tabpanel" class="tab-pane" id="video2">
+					<ul class="list-group">
+
+						<li class="list-group-item">Se realizó la restauración de material de la revolución mexicana, y se presentó en Pordenode Italia, en el marco de...<span class="see-more"><a href="#">Leer más</a></span></li>
+
+						<div class="video-preview">
+							<div class="embed-responsive embed-responsive-4by3">
+								<iframe width="420" height="315" src="https://www.youtube-nocookie.com/embed/LFu1pfcrDzQ" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</ul>
 				</div>
 			</div>
 		</div>
