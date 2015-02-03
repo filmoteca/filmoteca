66e0e17710270b46efb63b23f575275609352468
monyc2000 <monica.guzman.r@gmail.com>
2015-02-03 17:33:57 -0600

    Se rediseñó el menú principal

--- app/views/elements/menus/main.blade.php
+++ app/views/elements/menus/main.blade.php
@@ -1,16 +1,177 @@
+
+<nav class="navbar navbar-inverse" role="navigation">
 <div class="navbar-header">
-	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+<!-- 	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
 		<span class="sr-only">Toggle navigation</span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 	</button>
-</div>
+</div> -->
+<button type="button" class="navbar-toggle" data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+  </div>
 
-<div class="main-menu collapse navbar-collapse" id="main-menu">
-	<ul>
+<!-- <div class="main-menu collapse navbar-collapse" id="main-menu"> -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+	<ul class="nav navbar-nav">
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          QUIÉNES SOMOS<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/pages/quienes-somos/mision-y-vision">Misión y Visión</a></li>
+         
+          <li><a href="/pages/quienes-somos/objetivos-y-funciones">Objetivos y Funciones</a></li>
+        
+          <li><a href="/pages/quienes-somos/consejo-asesor">Consejo Asesor</a></li>
+          
+          <li ><a href="/pages/quienes-somos/memoria-filmoteca">Memoria Filmoteca</a></li>
+          
+          <li><a href="/pages/quienes-somos/cronologia">Cronología</a></li>
+       
+          <li ><a href="/pages/quienes-somos/medalla-filmoteca">Medalla Filmoteca</a></li>
+          
+          <li><a href="http://issuu.com/libro_filmo/docs/filmotecaunam_libro50anos">Libro Filmoteca: 50 años</a></li>
+         
+          <li><a href="/pages/quienes-somos/directorio">Directorio</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          ACERVO<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/pages/acervo/filmico">Fílmico</a></li>
+          
+          <li><a href="/pages/acervo/aparatos-antiguos">Aparatos antiguos</a></li>
+          
+          <li><a href="/pages/acervo/biblioteca">Biblioteca</a></li>
+          
+          <li><a href="/pages/acervo/restauracion">Restauración</a></li>
+          
+          <li ><a href="http://www.filmoteca.unam.mx/MUVAC">Museo virtual</a></li>
+          
+          <li><a href="http://www.filmoteca.unam.mx/cinelinea">Cine en línea</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          SERVICIOS<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/pages/servicios/banco-de-imagen">Banco de imagen</a></li>
+          
+          <li><a href="/pages/servicios/departamento-de-catalogacion">Catalogación</a></li>
+          
+          <li><a href="/pages/servicios/centro-de-documentacion">Centro de documentación</a></li>
+          
+          <li ><a href="/pages/servicios/deposito-y-resguardo">Depósito y resguardo</a></li>
+          
+          <li><a href="/pages/servicios/laboratorio-cinematografico">Laboratorio cinematográfico</a></li>
+          
+          <li><a href="/pages/servicios/prestamo-y-alquiler-de-peliculas">Préstamo y alquiler de películas</a></li>
+          
+          <li><a href="/pages/servicios/departamento-de-produccion">Producción</a></li>
+          
+          <li><a href="/pages/servicios/taller-de-restauracion">Taller de restauración</a></li>
+          
+          <li><a href="/pages/servicios/requisitos-para-solicitar-un-servicio">Requisitos para solicitar un servicio</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          PROGRAMACIÓN<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="{{ URL::to('exhibition') }}">Programación del mes</a></li>
+          
+          <li><a href="/pages/cartelera-digital">Cartelera digital</a></li>
+          
+          <li><a href="/pages/invitaciones">Invitaciones</a></li>
+          
+          <li ><a href="{{ URL::to('exhibition/history')}}">Histórico</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          DIFUSIÓN<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/press_register">Prensa</a></li>
+          
+          <li><a href="/pages/press/exposiciones">Exposiciones</a></li>
+          
+          <li><a href="/pages/press/publicaciones">Publicaciones</a></li>
+          
+          <li><a href="/news/index">Noticias</a></li>
+          
+          <li ><a href="/pages/press/filmoteca-in-the-media">Filmoteca en los medios</a></li>
+          
+          <li><a href="/pages/press/interview">Entrevistas</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          EXTENSIÓN ACADÉMICA<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/pages/extension-academica/cursos-y-talleres">Cursos y talleres</a></li>
+          
+          <li><a href="/pages/extension-academica/servicio-social">Servicio social</a></li>
+          
+          <li><a href="/pages/extension-academica/becarios">Becarios</a></li>
+          
+          <li ><a href="//pages/extension-academica/visitas-guiadas">Visitas guiadas</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          CONCURSOS<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="/pages/concursos/jose-rovirosa">José Rovirosa</a></li>
+          
+          <li><a href="/pages/concursos/alfonsoreyes">Alfonso Reyes "Fósforo</a></li>
+          
+          <li><a href="/pages/concursos/corto-movil">Corto Móvil</a></li>
+          
+          <li ><a href="/pages/concursos/convocatorias">Convocatorias</a></li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="/pages/quienes-somos/mision-y-vision" class="dropdown-toggle" data-toggle="dropdown">
+          LIBRERÍA<b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Catálogo</a></li>
+          
+          <li><a href="#">Venta en línea</a></li>
+          
+          <li><a href="#">Otros puntos de venta</a></li>
+        </ul>
+      </li>
+
+    </ul>
+
+<!-- 	<ul>
 		<li>
-			<a><span>QUIÉNES SOMOS</span></a>
+		<a href="/pages/quienes-somos/mision-y-vision">
+		<span>QUIÉNES SOMOS</span></a>
 			<ul class="submenu">
 				<li>
 					<a href="/pages/quienes-somos/mision-y-vision">
@@ -44,6 +205,11 @@
 					</a>
 				</li>
 				<li>
+	            	<a href="/pages/quienes-somos/medalla-filmoteca">
+						<span>Libro Filmoteca UNAM: 50 años</span>
+					</a>
+				</li>
+				<li>
 					<a href="/pages/quienes-somos/directorio">
 						<span>Directorio</span>
 					</a>
@@ -52,28 +218,41 @@
 		</li>
 
 		<li>
-			<a href="/pages/acervos/index"><span>ACERVO</span></a>
+		<a href="/pages/acervo/acervo-filmico"><span>ACERVO</span></a>
 	        <ul class="submenu">
+				
 				<li>
-					<a href="/pages/acervo/resguardo-y-conservacion">
-						<span>Resguardo y conservación</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span>Acervo fílmico</span>
+					<a href="/pages/acervo/acervo-filmico">
+						<span>Fílmico</span>
 					</a>
 				</li>
 				<li>
 					<a href="/pages/acervo/aparatos-antiguos">
-						<span>Acervo de aparatos antiguos</span>
+						<span>Aparatos antiguos</span>
 					</a>
 				</li>
 				<li>
-					<a href="http://www.filmoteca.unam.mx/MUVAC">
-						<span>Museo virtual de aparatos cinematográficos</span>
+					<a href="#">
+						<span>Biblioteca</span>
 					</a>
 				</li>
+				<li>
+					<a href="/pages/acervo/resguardo-y-conservacion">
+						<span>Restauración</span>
+					</a>
+				</li>
+				<li>
+					<a href="/pages/acervo/historias-recuperadas">
+						<span>Historias recuperadas</span>
+					</a>
+				</li>
+		
+				<li>
+					<a href="http://www.filmoteca.unam.mx/MUVAC">
+						<span>Museo virtual</span>
+					</a>
+				</li>
+				
 				<li>
 					<a href="http://www.filmoteca.unam.mx/cinelinea">
 						<span>Cine en línea</span>
@@ -82,14 +261,10 @@
 	        </ul>
 		</li>
 
-	      <li>
-	      	<a href="#"><span>SERVICIOS</span></a>
+	    <li>
+	    <a><span>SERVICIOS</span></a>
 	        <ul class="submenu">
-				<li>
-					<a href="/pages/servicios/requisitos-para-solicitar-un-servicio">
-						<span>Requisitos para solicitar un servicio</span>
-					</a>
-				</li>
+				
 				<li>
 					<a href="/pages/servicios/banco-de-imagen">
 						<span>Banco de imagen</span>
@@ -115,18 +290,24 @@
 					<a href="/pages/servicios/laboratorio-cinematografico">
 						<span>Laboratorio cinematográfico</span>
 					</a>
-	            </li>
+	            </li> -->
+
+
+
 	            <!-- <li>
 					<a href="/pages/servicios/laboratorio-digital">
 						<span>Laboratorio digital</span>
 					</a>
 	             </li> -->
-				<li>
+		<!-- 		<li>
 					<a href="/pages/servicios/exposiciones-museografia">
 						<span>Museografía</span>
 					</a>
-				</li>
-	            <li>
+				</li> -->
+
+
+
+<!-- 	            <li>
 					<a href="/pages/servicios/prestamo-y-alquiler-de-peliculas">
 						<span>Préstamo y alquiler de películas</span>
 					</a>                          
@@ -141,20 +322,23 @@
 						<span>Taller de restauración</span>
 					</a>
 	           	</li>
+	           	<li>
+					<a href="/pages/servicios/requisitos-para-solicitar-un-servicio">
+						<span>Requisitos para solicitar un servicio</span>
+					</a>
+				</li>
 	        </ul>
 		</li>
 		<li>
-			<a href="{{ URL::to('exhibition/index') }}">
-				<span>PROGRAMACIÓN</span>
-			</a>
+		<a><span>PROGRAMACIÓN</span></a>
 	        <ul class="submenu">
 				<li>
-					<a href="{{ URL::to('exhibition/history')}}">
-						<span>Histórico de programación</span>
+					<a href="{{ URL::to('exhibition') }}">
+						<span>Programación</span>
 					</a>
 				</li>
 				<li>
-					<a href="/pages/cartelera-digital">
+					<a href="/billboard/send">
 						<span>Cartelera digital</span>
 					</a>
 				</li>
@@ -163,14 +347,33 @@
 						<span>Invitaciones</span>
 					</a>
 				</li>
+				<li>
+					<a href="{{ URL::to('exhibition/history')}}">
+						<span>Histórico de programación</span>
+					</a>
+				</li>
 			</ul>
 		</li>
 
 		<li>
-			<a>
-				<span>PRENSA</span>
-			</a>
+		<a><span>DIFUSIÓN</span></a>
 	        <ul class="submenu">
+				<li>
+					<a href="/press_register">
+						<span>Prensa</span>
+					</a>
+				</li>
+				<li>
+					<a href="/pages/servicios/exposiciones-museografia">
+						<span>Exposiciones</span>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span>Publicaciones</span>
+					</a>
+				</li>
+
 				<li>
 					<a href="/news/index">
 						<span>Noticias</span>
@@ -183,44 +386,47 @@
 					</a>
 				</li>
 				<li>
-					<a href="/pages/press/gallery">
-						<span>Galeria</span>
-					</a>
-				</li>
-				<li>
 					<a href="/pages/press/interview">
 						<span>Entrevistas</span>
 					</a>
 				</li>
 				<li>
-					<a href="/press_register">
-						<span>Atención a medios</span>
+					<a href="/pages/press/gallery">
+						<span>Galeria</span>
 					</a>
 				</li>
 	        </ul>
 
 		</li>
 		<li>
-			<a> 
-				<span>EXTENSIÓN ACADÉMICA</span>
- 			</a>
-			<ul class="submenu">
+		<a><span>EXTENSIÓN ACADÉMICA</span></a>
+			<ul class="submenu"> -->
+
+
 				<!-- <li>
 					<a href="/pages/extension-academica/procedimiento-de-inscripcion">
 						<span>Procedimiento de inscripción</span>
 					</a>
 				</li> -->
-				<li>
-					<a href="/pages/extension-academica/cursos-y-talleres">
+
+
+
+<!-- 				<li>
+					<a href="/courses/app#/">
 						<span>Cursos y talleres</span>
 					</a>
-				</li>
+				</li> -->
+
+
 				<!-- <li>
 					<a href="/pages/extension-academica/atencion-a-alumnos">
 						<span>Atención a alumnos</span>
 					</a>
 				</li> -->
-				<li>
+
+
+
+<!-- 				<li>
 					<a href="/pages/extension-academica/servicio-social">
 						<span>Servicio social</span>
 					</a>
@@ -239,20 +445,19 @@
 		</li>
 
 		<li>
-			<a href="#">
-				<span>CONCURSOS</span>
-			</a>
+		<a><span>CONCURSOS</span></a>	
 			<ul class="submenu">
-				<li>
-					<a href="/pages/concursos/alfonsoreyes">
-						<span>Alfonso Reyes "Fósforo"</span>
-					</a>
-				</li>
 				<li>
 					<a href="/pages/concursos/jose-rovirosa">
 						<span>José Rovirosa</span>
 					</a>
 				</li>
+				<li>
+					<a href="/pages/concursos/alfonsoreyes">
+						<span>Alfonso Reyes "Fósforo"</span>
+					</a>
+				</li>
+
 	            <li>
 					<a href="/pages/concursos/corto-movil">
 						<span>Corto Móvil</span>
@@ -268,9 +473,25 @@
 		</li>
 
 		<li>
-			<a href="http://cineunam.librosdelau.com/">
-				<span>LIBRERÍA Y TIENDA</span>
-			</a>
+		<a><span>LIBRERÍA</span></a>
+			<ul class="submenu">
+				<li>
+					<a href="/shop">
+						<span>Catálogo</span>
+					</a>
+				</li>
+				<li>
+					<a href="http://cineunam.librosdelau.com/">
+						<span>Venta en línea</span>
+					</a>
+				</li>
+
+	            <li>
+					<a href="/pages/concursos/corto-movil">
+						<span>Otros puntos de venta</span>
+					</a>
+				</li> 
+	        </ul> 
 		</li>
-	</ul>
+	</ul> -->
 </div>
