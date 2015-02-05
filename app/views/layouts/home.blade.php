1e87426595f4d95630bc4282de1c9c7742d5585c
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-02-04 20:34:32 -0600

    Removi validaciones de local y prod en la página de inicio.

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -6,7 +6,6 @@
 
 		@yield('metas')
 
-		@if (App::isLocal() )
 
 		{{ HTML::style('/assets/css/filmoteca.css') }}
 
@@ -16,17 +15,7 @@
 				'/bower_components/bootstrap/dist/js/bootstrap.min.js',
 				'/bower_components/slick.js/slick/slick.min.js'
 				])
-			}}
-
-		@else
-			{{--
-				La idea de esta sección es cargar, cuando se está en producción,
-				un unico archivo css. Sin embargo hay dudas si se deben
-				concatenar únicamente los archivos propios sin los css de terceros
-				o concatenar todo.
-				--}}
-				{{ HTML::style('/assets/css/filmoteca.min') }}
-		@endif
+		}}
 
 		@yield('styles')
 
