0228462644c55ef806f2764c19b1ee0e73d1b423
monyc2000 <monica.guzman.r@gmail.com>
2015-02-22 18:44:01 -0600

    Nueva página de cartelera

---
+++ app/views/pages/programacion/cartelera-digital.blade.php
@@ -0,0 +1,108 @@
+
+@section('breadcrumbs')
+	<li>
+		<a href="/pages/cartelera-digital/index">
+			Programacion
+		</a>
+	</li>
+	<li class="active">
+		Cartelera digital
+	</li>
+@stop
+
+
+@section('sidebar')
+	@include('elements.menus.programacion', array('selected' => 1))
+@stop
+
+
+@section('content')
+
+
+      <div class="col-xs-12 col-sm-12">
+         
+         <h3>CARTELERA DIGITAL</h3>
+
+          <div class="row">
+
+            <div class="col-12 col-sm-12 col-lg-12">
+              <h5>MARZO 2015</h5>
+                <div class="col-sm-12">
+                  <img src="/imgs/cursos/orson-welles.jpg">
+                </div>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank" align>Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+          </div><!--/row-->
+          <br><br>
+
+          <div class="row">
+           <h5>CARTELERAS ANTERIORES 2015</h5>
+            <div class="col-3 col-sm-4 col-lg-3">
+              <p>Febrero</p>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'La mancha de sangre'></a>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank">Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+            <div class="col-3 col-sm-4 col-lg-3">
+            <p>Enero</p>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'La mancha de sangre'></a>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank">Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+          </div><!--/row-->
+
+
+           <br><br>
+          <div class="row">
+           <h5>CARTELERAS ANTERIORES 2014</h5>
+            <div class="col-3 col-sm-4 col-lg-3">
+              <p>Diciembre</p>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'La mancha de sangre'></a>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank">Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+            <div class="col-3 col-sm-4 col-lg-3">
+            <p>Noviembre</p>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'La mancha de sangre'></a>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank">Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+            <div class="col-3 col-sm-4 col-lg-3">
+            <p>Enero</p>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'La mancha de sangre'></a>
+              <div class="col-sm-12">
+                <br>
+                <a href="" target="_blank">Descarga / </a>
+                <a href="" target="_blank"> Consulta</a>
+              </div>
+            </div><!--/span-->
+
+          </div><!--/row-->
+
+        </div><!--/span-->
+ 
+
+
+		
+@stop
