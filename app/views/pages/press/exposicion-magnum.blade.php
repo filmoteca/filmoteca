a3eb6094369d689c33839ec38f621fa25493d925
monyc2000 <monica.guzman.r@gmail.com>
2015-03-01 18:19:15 -0600

    se actualizó el plugin del carrusel y se ambiaron detalles de texto

--- app/views/pages/press/exposicion-magnum.blade.php
+++ app/views/pages/press/exposicion-magnum.blade.php
@@ -1,9 +1,10 @@
 @section('scripts')
 
-<script type='text/javascript' src='/bower_components/slider-camera/jquery.min.js'></script>
-<script type='text/javascript' src='/bower_components/slider-camera/jquery.mobile.customized.min.js'></script>
-<script type='text/javascript' src='/bower_components/slider-camera/jquery.easing.1.3.js'></script> 
-<script type='text/javascript' src='/bower_components/slider-camera/camera.min.js'></script> 
+<!-- This plugin requires jquery 1.7 to work. This jQuery's version is packaged with slider-camera plugin. -->
+<script type='text/javascript' src='/bower_components/slider-camera/scripts/jquery.min.js'></script>
+<script type='text/javascript' src='/bower_components/slider-camera/scripts/jquery.easing.1.3.js'></script>
+<script type='text/javascript' src='/bower_components/slider-camera/scripts/jquery.mobile.customized.min.js'></script>
+<script type='text/javascript' src='/bower_components/slider-camera/scripts/camera.min.js'></script>
 
 <script>
   jQuery(function(){
@@ -27,7 +28,7 @@
 		</a>
 	</li>
 	<li class="active">
-		Exposiciones / Exposiciones realizadas
+		Exposiciones / Exposiciones recientes
 	</li>
 @stop
 
@@ -79,27 +80,14 @@
 <!-- <div class="img-servicios">
 		<img src="/imgs/historias-recuperadas/dracula.jpg" class="img-responsive" 'Drácula' aling="left">
 	</div> -->
-
-	
-
+    <h5>Cédula</h5>
 	<p>Fundada en París el 22 de mayo de 1947 por los célebres fotógrafos Henri Cartier-Bresson, Robert Capa, David Seymour y George Rodger, la agencia Magnum fue creada como una cooperativa, con el propósito de proteger los derechos de los fotógrafos sobre sus obras y darles independencia económica y libertad de creación, sin ningún tipo de censura. Abierta a todas las nacionalidades y a todas las tendencias, al año siguiente de su fundación instalaba también un despacho en Nueva York, realizando foto-reportajes en todo el mundo.<p>
 
 	<p>Actualmente la agencia cuenta además con oficinas en Londres y Japón. Entre Magnum y el cine se establece una vigorosa relación que tiene como centro el Hollywood de finales de los cuarenta y los cincuenta, hasta el gran éxito de la televisión, que empieza en esos años a sustituir a las revistas como el medio ideal para la promoción de las películas. Entonces es Europa, en particular en el seno de la Nueva ola francesa, donde los fotógrafos de Magnum tienen la oportunidad de mantenerse ligados a este campo durante algunos años más. La agresividad de nuevas agencias especializadas en la cinefotografía, que exigen exclusividad durante el tiempo de rodaje, contrasta con la independencia de Magnum y la libertad de elección para el fotógrafo, lo que hace que cada vez tenga más dificultad para trabajar en el cine. Raymond Depardon, Wayne Miller, Guy Le Querrec, Philippe Halsman, Patrick Zachmann, Martine Franck Dennis Stock, Gilles Peress, Eve Arnold, Elliott Erwitt, Erich Hartmann, Marc Riboud, Ferdinando Scianna, Sebastião Salgado y Steve McCurry son sólo algunos de los fotógrafos de la agencia que han documentado importantes aspectos de la vida moderna a través de sus lentes. La excelencia alcanzada por los integrantes de Magnum ha tenido una extraordinaria influencia en la historia de la fotografía y del periodismo gráfico.</p>
 
 	<p>Este año la Filmoteca de la UNAM conmemora el 10º aniversario luctuoso de Henri Cartier-Bresson (1908-2004) con la presente exposición que exhibe un amplio panorama de la cinematografía francesa plasmado por los fotógrafos de la Agencia Magnum, con la presencia de las figuras fundamentales en el desarrollo de esta industria fílmica, una de las más importantes a nivel mundial. Realizadores y actores de distintas épocas figuran en la muestra, con un énfasis particular en los creadores y personajes del movimiento Nouvelle vague (nueva ola).</p>
 
-	<p>La exposición consta de 20 reproducciones de carteles, originalmente montados en bastidores con acrílicos, de 60 x 90 cm. Todo el material está digitalizado para ofrecerlo a sedes foráneas.</p>
-
-	<p>Te invitamos a conocer, a través de nuestra página web www.filmoteca.unam.mx, el resto de las actividades de extensión académica que realiza la Filmoteca de la UNAM, o visita directamente nuestras instalaciones en Circuito Maestro Mario de la Cueva s/n, frente a la Facultad de Ciencias Políticas y Sociales, en Ciudad Universitaria.</p>
-
-	
-
-
- 
-
-
-
-
-
+    <h5>Detalles</h5>
+    <p>Esta exposición consta de 20 reproducciones de carteles, originalmente montados en bastidores con acrílicos, de 60 x 90 cm. <strong> Todo el material está digitalizado para ofrecerlo a sedes foráneas.</strong></p>
 
 @stop
