a0f580c888c0f083dc6942c7c986d95a1fa084ce
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-03-08 22:38:04 -0600

    [#64]Creación de la página de las carteleras

---
+++ app/views/frontend/billboards/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.default')
+
+@section('breadcrumbs')
+    <li>
+        <a href="/pages/cartelera-digital/index">
+            Programacion
+        </a>
+    </li>
+    <li class="active">
+        Cartelera digital
+    </li>
+@stop
+
+
+@section('sidebar')
+    @include('elements.menus.programacion', array('selected' => 1))
+
+    <div class="subscribe-box">
+
+        <p>Recibe nuestra cartelera digital</p>
+
+        <div class="input-group input-group-sm">
+            <input type="email" 
+                name="email" 
+                placeholder="Ingresa tu correo electrónico"
+                class="form-control">
+            <span class="input-group-addon">@</span>
+        </div>
+
+        <button type="button" class="btn btn-success">Enviar</button>
+    </div>
+@stop
+
+
+@section('content')
+
+<h3 class="text-uppercase">Cartelera digital</h3>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12">
+        <h5 class="text-uppercase">
+            Cartelera de {{ trans('dates.months.' . date('F', strtotime($lastBillboard->created_at))) }}
+        </h5>
+        <p>
+            <img src="{{ asset( $lastBillboard->image->url('thumbnail')) }}" 
+                class="img-responsive image-size-icon" 
+                title="{{ trans('dates.months.' . date('F', strtotime($lastBillboard->created_at))) }}" 
+                alt="{{ trans('dates.months.' . date('F', strtotime($lastBillboard->created_at))) }}">
+        </p>
+        <p>
+            <a href="{{ $lastBillboard->pdf->url() }}" target="_blank" align>Descarga / </a>
+            <a href="{{ $lastBillboard->online_version_url }}" target="_blank">Consulta</a>
+        </p>
+    </div>
+</div>
+
+    
+@foreach($earlierBillboards as $year => $billboards)
+    
+    <div class="row">
+        <h3>Cateleras del año {{$year}}</h3>
+    @foreach($billboards as $billboard)
+        <div class="col-xs-3 col-sm-3">
+            <h5 class="text-uppercase">
+                {{ trans('dates.months.' . date('F', strtotime($billboard->created_at))) }}
+            </h5>
+            <p>
+                <img src="{{ asset( $billboard->image->url('thumbnail')) }}" 
+                    class="img-responsive image-size-icon" 
+                    title="{{ trans('dates.months.' . date('F', strtotime($billboard->created_at))) }}" 
+                    alt="{{ trans('dates.months.' . date('F', strtotime($billboard->created_at))) }}">
+            </p>
+            <p>
+                <a href="{{ $billboard->pdf->url() }}" target="_blank" align>Descarga / </a>
+                <a href="{{ $billboard->online_version_url }}" target="_blank">Consulta</a>
+            </p>
+        </div>
+    @endforeach
+
+    </div>
+@endforeach
+
+@stop
