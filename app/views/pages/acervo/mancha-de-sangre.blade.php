54f2bbe949433091e7bf52a5ec01f0df0ccc9ccb
monyc2000 <monica.guzman.r@gmail.com>
2015-03-16 20:56:47 -0600

    de agregó la clase uppercase para cambiar los titulares

--- app/views/pages/acervo/mancha-de-sangre.blade.php
+++ app/views/pages/acervo/mancha-de-sangre.blade.php
@@ -36,7 +36,7 @@
           <div class="row">
 
             <div class="col-6 col-sm-6 col-lg-6">
-              <h4>LA MANCHA DE SANGRE</h4>
+              <h4 class="uppercase">La mancha de sangre</h4>
               <img src="/imgs/historias-recuperadas/la-mancha-de-sangre.jpg" class="img-responsive" 'La mancha de sangre'></a>
               <p>Filme perdido durante cincuenta años. El negativo original de nitrato de celulosa lo encontramos maltrecho, incompleto y muy rayado.  Los censores, que durante los años treinta secuestraron el filme y lo mutilaron, no solamente lo cortaron sino que lo manipularon de la peor manera:
               </p>
@@ -44,14 +44,14 @@
             </div><!--/span-->
 
             <div class="col-6 col-sm-6 col-lg-6">
-              <h4>DRÁCULA</h4>
+              <h4 class="uppercase">Drácula</h4>
               <img src="/imgs/historias-recuperadas/dracula.jpg" class="img-responsive" 'Drácula'></a>
               <p>Como si fueran objetos inservibles y sin valor comercial, los negativos originales de las películas filmadas en todo el mundo se han perdido con pasmosa facilidad.  Así le sucedió a este filme que se consideró desaparecido hasta principios de los años 90; año en que se localizó una copia de nitrato de celulosa de esta película en Cuba.</p>
               <p><a class="btn btn-default" href="/pages/acervo/dracula" role="button">Leer más »</a></p>
             </div><!--/span-->
 
             <div class="col-6 col-sm-6 col-lg-6">
-              <h4>LÍMITE</h4>
+              <h4 class="uppercase">Límite</h4>
               <img src="/imgs/historias-recuperadas/limite.jpg" class="img-responsive" 'Limite'></a>
               <p>Varios son los intentos que se han hecho para restaurar este filme. Conocemos, por ejemplo, la versión que fue lanzada en 1978 y exhibida en el Congreso de FIAF en Oaxtepec, Morelos, México, cuyo Simposio llevó como tema “El Cine Olvidado de América Latina”.  Sin embargo, ahora existe otra versión,
              </p>
@@ -59,7 +59,7 @@
             </div><!--/span-->
 
             <div class="col-6 col-sm-6 col-lg-6">
-              <h4>LAS VIÑAS DE LA IRA</h4>
+              <h4 class="uppercase">Las viñas de la ira</h4>
               <img src="/imgs/historias-recuperadas/vinas-de-la-ira.jpg" class="img-responsive" 'Las vñas de la ira'></a>
               <p>Hoy consideramos a John Ford como el mejor director de westerns que ha habido en toda la historia, pero esto no siempre fue así. Desde mediados de los años treinta hasta que los Estados Unidos se involucraron en la Segunda Guerra Mundial, Ford dirigió una serie de dramas sociales que respondían muy puntualmente a la difícil situación por la que atravesaba su país. </p>
               <p><a class="btn btn-default" href="/pages/acervo/vinas-de-la-ira" role="button">Leer más »</a></p>
