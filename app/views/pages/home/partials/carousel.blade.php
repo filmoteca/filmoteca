bce6b70e75f80641b3ea3fc55935eadcf30d44d0
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-05-18 10:53:59 -0500

    Se verifica que los carruseles existan antes de dibujarlos

--- app/views/pages/home/partials/carousel.blade.php
+++ app/views/pages/home/partials/carousel.blade.php
@@ -1,25 +1,27 @@
 <div class="flm-section carousel">
     <div class="content">
         <div class="main-carousel">
-            @foreach ($carousels['home']->carousel_images as $image)
-                <div>
-                    <div class="caption">
-                        <div id="title">
-                            {{ $image->title }}
+            @if (isset($carousels['home']))
+                @foreach ($carousels['home']->carousel_images as $image)
+                    <div>
+                        <div class="caption">
+                            <div id="title">
+                                {{ $image->title }}
+                            </div>
+                            <br>
+                            <div id="text">
+                                {{ $image->description }}
+                            </div>
                         </div>
-                        <br>
-                        <div id="text">
-                            {{ $image->description }}
-                        </div>
+                        <a href="{{ $image->link }}">
+                            <img src="{{ Config::get('administrator::administrator.cms_upload_url') . $image->image }}"
+                                 alt="{{ $image->title }}"
+                                 title="{{ $image->description }}"
+                                 class="img-responsive">
+                        </a>
                     </div>
-                    <a href="{{ $image->link }}">
-                        <img src="{{ Config::get('administrator::administrator.cms_upload_url') . $image->image }}"
-                             alt="{{ $image->title }}"
-                             title="{{ $image->description }}"
-                             class="img-responsive">
-                    </a>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
         </div>
     </div>
    
--- app/views/pages/home/partials/recommendations.blade.php
+++ app/views/pages/home/partials/recommendations.blade.php
@@ -5,16 +5,18 @@
 		</div>
 
 		<div class="visit-carousel">
-            @foreach ($carousels['recomendaciones']->carousel_images as $image)
-                <div>
-                    <a href="{{ $image->link }}" target="blank">
-                        <img src="{{ Config::get('administrator::administrator.cms_upload_url') . $image->image }}"
-                             alt="{{ $image->title }}"
-                             title="{{ $image->description }}"
-                             class="img-responsive">
-                    </a>
-                </div>
-            @endforeach
+            @if (isset($carousels['recomendaciones']))
+                @foreach ($carousels['recomendaciones']->carousel_images as $image)
+                    <div>
+                        <a href="{{ $image->link }}" target="blank">
+                            <img src="{{ Config::get('administrator::administrator.cms_upload_url') . $image->image }}"
+                                 alt="{{ $image->title }}"
+                                 title="{{ $image->description }}"
+                                 class="img-responsive">
+                        </a>
+                    </div>
+                @endforeach
+            @endif
 		</div>
 	</div>
 </div>
