8cf18fe3d1fe4adaeed8958b7054db652b452035
monyc2000 <monica.guzman.r@gmail.com>
2015-06-09 05:19:49 -0500

    Paginas nuevas

---
+++ app/views/pages/concursos/convocatorias-concurso-videoarte.blade.php
@@ -0,0 +1,148 @@
+@section('breadcrumbs')
+	<li>
+		<a href="/pages/concursos/index">
+			Concursos
+		</a>
+	</li>
+	<li class="active">
+		Convocatorias
+	</li>
+@stop
+
+@section('sidebar')
+	@include('elements.menus.concursos', array('selected' => 3))
+@stop
+
+@section('content')
+
+	<h1>CONCURSO DE VIDEOARTE</h1>
+
+		<p align="center">La Universidad Nacional Autónoma de México a través del Museo Universitario Arte Contemporáneo, la Facultad de Ciencias Políticas y Sociales y la Dirección General de Actividades Cinematográficas, Filmoteca UNAM</p>
+		<p align="center"><strong>CONVOCAN</strong></p>
+
+		<p align="center">al <strong>Concurso Nacional de Videoarte Universitario Visiones del arte</strong></p>
+
+		<p align="right"><i>Se podría definir formalmente al videoarte como cualquier obra con intencionalidades artísticas que emplea, en sus procesos y como soporte, a los instrumentos o dispositivos propios del video.</i></p>
+		<p align="right"><i>Gilles Charalambos</i>
+
+		<p align="right"><i>El cine sería la épica, la televisión la novela y el videoarte la poesía</i></p>
+		<p align="right"><i>Pola Weiss</i></p>
+
+		
+		<p>La masificación de la cámara de video expandió las posibilidades narrativas y formales de la producción artística. La capacidad de registrar el mundo y manipular material audiovisual permitió conformar lenguajes y lógicas de creación alternativas a la de la cultura de masas, regulada por el cine y la televisión. Hoy en día, cinco décadas después, el video se ha convertido en una herramienta al alcance de la mayoría con la aparición de cámaras miniatura en dispositivos móviles y plataformas de distribución de videos caseros en los medios electrónicos que han reformulado las condiciones de realización de video en la actualidad.</p>
+
+		<p>Esta convocatoria tiene la intención de contribuir al estímulo de la producción de videoarte en México iniciada en los años setenta, impulsando a los nuevos creadores y abriendo un espacio de encuentro entre artistas de distintas generaciones para seguir reflexionando sobre los significados y posibilidades que plantea esta práctica.</p>
+
+		<p>Para esta 5a edición te invitamos a realizar una pieza de videoarte en la que destaquen la reflexión, experimentación y creación artística a partir de los procesos y soportes videográficos.</p>
+
+		<p><strong>PARTICIPANTES</strong></p>
+			<ul>
+				<li>Esta convocatoria está abierta a estudiantes de bachillerato, licenciaturas y posgrados, así como a personas o artistas de formación independiente interesadas en la creación artística a través del video.</li>
+				<li>Se recibirán trabajos individuales y colectivos.</li>
+				<li>Cada autor o colectivo podrá presentar un máximo de 3 obras.</li>
+			</ul>
+		 
+
+		<p><strong>CATEGORÍAS</strong></p>
+			<ul>
+				<li>Preparatorias y Bachilleratos.</li>
+				<li>Licenciaturas y Posgrados.</li>
+				<li>Formación independiente (Productores o realizadores autodidactas o con una formación no institucional).</li>
+			</ul>
+		 
+
+		<p><strong>OBRAS</strong></p>
+			<ul>
+				<li>Es requisito que los trabajos sean originales y no hayan sido proyectados con anterioridad.</li>
+				<li>Los trabajos deberán tener una duración máxima de 10 minutos.</li>
+				<li>Los videos deberán subirse a un canal de Youtube o Vimeo personal en configuración PRIVADO en formato HD 1920 x 1080 (16:9).</li>
+				<li>El link de visualización deberá ser enviado junto con la contraseña de acceso al correo  concurso.videoarte@muac.unam.mx</li>
+				<li>Todos los trabajos deberán incluir una cortinilla en letra Arial no. 12 con la siguiente información: nombre(s) autor(es), título y categoría.</li>  
+			</ul>
+
+
+		<p><strong>INSCRIPCIÓN</strong></p>
+			<ul>
+				<p>Cada trabajo registrado deberá enviarse al correo  concurso.videoarte@muac.unam.mx incluyendo:</p>
+				<li>Ficha de inscripción completa en formato pdf (descarga la ficha en la página del MUAC).</li>
+				<li>Link privado al video en canal de Youtube o Vimeo con contraseña de acceso.</li>
+				<li>2 imágenes still del video (tiff, png, jpeg, 300 dpi) con fines de difusión.</li>
+				<li>NOTA: cada pieza deberá enviarse en un correo electrónico diferente.</li>
+				<li>La convocatoria estará abierta del 1 de junio al 30 de octubre del 2015.</li>
+			</ul>
+		 
+
+		<p><strong>SELECCIÓN</strong></p>
+			<ul>
+				<li>Los trabajos serán seleccionados por un comité calificador integrado por artistas, académicos y profesionales del video de reconocida trayectoria.</li>
+				<li>El fallo del jurado será inapelable y se dará a conocer el día de la premiación.</li>
+				<li>Se nombrará un primer lugar en cada categoría así como menciones honoríficas (a consideración del jurado).</li>
+			</ul>
+		 
+
+		<p><strong>PREMIOS</strong></p>
+
+				<p><strong>1° Lugar de cada categoría</strong></p>
+			<ul>
+				<li>Cámara GoPro HD HERO3 White Edition (Se entregará una cámara, ya sea trabajo colectivo o individual).</li>
+				<li>Taller Final Cut Pro impartido por Corporativo Simplemente. Duración: 20 horas.</li>
+				<li>Taller Apreciación cinematográfica impartido por Filmoteca. Duración: 20 horas.</li>
+				<li>Proyección de los trabajos en una gira universitaria:
+					<ul>
+						<li>Sala Carlos Monsiváis (CCU).</li>
+						<li>Cinematógrafo del Chopo (Museo Universitario del Chopo).</li>
+						<li>Sala Lumiére (Casa del Lago).</li>
+						<li>Al aire libre, Facultad de Ciencias Políticas y Sociales UNAM.</li>
+					</ul>
+				</li>
+				<li>Muestra de los trabajos en Centro Cultural Border.</li>
+				<li>Publicaciones de MUAC / FCPyS / Filmoteca UNAM.</li>
+
+			</ul>
+
+
+			<p>Los trabajos premiados con <strong>Mención honorífica</strong> de cada categoría recibirán:</p>
+			<ul>
+				<li>Taller Apreciación cinematográfica impartido por Filmoteca. Duración: 20 horas.</li>
+				<li>Proyección de los trabajos en una gira universitaria:
+					<ul>
+						<li>Sala Carlos Monsiváis (CCU).</li>
+						<li>Cinematógrafo del Chopo (Museo Universitario del Chopo).</li>
+						<li>Sala Lumiére (Casa del Lago).</li>
+						<li>Al aire libre, Facultad de Ciencias Políticas y Sociales UNAM.</li>
+					</ul>
+				</li>
+				<li>Muestra de los trabajos en Centro Cultural Border.</li>
+				<li>Publicaciones de MUAC / FCPyS / Filmoteca UNAM.</li>
+			</ul>
+		 
+
+		<p><strong>ESPECIFICACIONES</strong></p>
+			<ul>
+				<li>Los autores de los videos premiados de cada categoría deberán entregar 3 copias del trabajo con las especificaciones que se indiquen en ese momento.</li>
+				<li>El o los autores de las obras ganadoras ceden los derechos de exhibición y difusión de la misma a la UNAM con el fin de ser utilizadas para la promoción y difusión del arte, la cultura y la academia, sin fines de lucro.</li>
+				<li>El hecho de participar en el concurso supone la aceptación plena de estas bases.</li>
+				<li>Los casos no previstos en esta convocatoria serán resueltos por los organizadores.</li>
+			</ul>
+
+		<p><strong>CONTACTO E INFORMES</strong></p>
+
+		<p>Correo electrónico: <a href="mailto:concurso.videoarte@muac.unam.mx">concurso.videoarte@muac.unam.mx</a></p>
+
+		<p>Museo Universitario Arte Contemporáneo – MUAC</p>
+
+		<p>Insurgentes Sur 3000, Centro Cultural Universitario, Delegación Coyoacán, C.P. 04510. Teléfono: 56226999 ext. 48829 | twitter: @publicosmuac | facebook: Educación muac</p>
+
+		<p>Extensión Universitaria de la Facultad de Ciencias Políticas y Sociales de la UNAM</p>
+
+		<p>Circuito Mario de la Cueva S/N, Ciudad Universitaria, Delegación Coyoacán.</p>
+
+		<p>Teléfono: 5622 9470, ext. 1030 y 1052 | twitter: @ExtensionFCPyS | facebook: Facultad de Ciencias Políticas y Sociales-UNAM</p>
+
+		<p>Dirección General de Actividades Cinematográficas, Filmoteca UNAM</p>
+
+		<p>Circuito Maestro Mario de la Cueva s/n. Ciudad Universitaria, Delegación Coyoacán.</p>
+
+		<p>Teléfono: 5622 9589 | twitter: @ButacaUNAM | Facebook: Butaca Unam</p>
+
+@stop
---
+++ app/views/pages/cursos/guion-documental.blade.php
@@ -0,0 +1,195 @@
+@section('breadcrumbs')
+<li>
+  <a href="/pages/extension-academica/index">
+    Extensión Academica
+  </a>
+</li>
+<li class="active">
+  Cursos y talleres.
+</li>
+@stop
+
+
+@section('sidebar')
+  @include('elements.menus.extension-academica', array('selected' => 0))
+@stop
+
+
+
+@section('content')
+
+	<div class="img-servicios">
+		<img src="/imgs/cursos/taller-guion-documental.jpg" class="img-responsive" 'Fundamentos de guión para cine documental' aling="left">
+	</div>
+
+	<h1>Cursos de la Filmoteca de la UNAM</h1>
+  <br>
+  <h2>TALLER FUNDAMENTOS DE GUIÓN PARA CINE DOCUMENTAL</h2>
+   <strong><p>Impartido por: José Luis Mariño López</p>
+</strong>
+
+
+    <p align="right"><i>El guión es escritura y reescritura. Más allá de ser la primera etapa de una película, es el eje de ésta y sigue un proceso permanente de re-escritura en todas las fases de producción.
+    Esto porque la fotografía y la cámara narran, porque se escribe con la luz, se cuenta con ambientes y atmósferas, con ruidos y sonidos... Es decir, el guión es una escritura que no concluye cuando se pone fin en el impreso sino cuando una estética le da forma a la narración allí planteada, la reinventa.</i></p>
+    <p align="right">Lucía Victoria Torres</p>
+    <br>
+
+    <p>Éste es un taller introductorio cuya meta general es presentar y desarrollar en la práctica elementos fundamentales del guión para cine documental, entendiendo al guión como al parte esencialmente creativa, de desarrollo de posibilidades, de exploración de escenarios y de empleo de recursos de la preproducción de una película documental, que no sólo debe cubrir apropiadamente los requisitos técnicos, logísticos y metodológicos; también debe sondear -y a ello se dedica este taller- las perspectivas centrales y derivadas del tema elegido, las ideas visuales y sonoras, el diseño de la profundización temática, la composición de la narrativa, el ritmo, los sujetos protagónicos, las estructuras discursivas; en suma, esbozar el estilo y función de la obra, sea que ésta se adhiera a un esquema preciso de rodaje o cuya resolución principal se dé en el proceso de edición - postproducción.</p>
+    <br>
+
+    <p align="right"><i>Al guión podríamos llamarlo también una guía grande, abierta a las sucesivas reescrituras que ocurren cuando la investigación avanza, cuando acontece el registro, cuando se piensa en montaje, cuando el público debate y rediscute el film.</p></i>
+    <p align="right">Claudio Remedi</p>
+
+
+    <p><strong>Requisito:</strong> contar un tema definido para desarrollar durante el Taller.</p>
+
+    <p><strong>OBJETIVOS GENERALES</p></strong>
+
+        <ul>
+          <li>Conocimiento del guión para cine documental y aprendizaje de su teoría, práctica y técnicas.</li>
+          <li>Familiarización con los esquemas básicos para guión de cine documental.</li>
+          <li>Apreciación de las estructuras organizativas y narrativas en diversos ejemplos de estilos de cine documental.</li>
+          <li>Introducción a la lectura profunda de películas documentales.</li>
+        </ul>
+
+    <p><strong>OBJETIVOS ESPECÍFICOS</p></strong>
+
+        <ul>
+          <li>Planteamiento y desarrollo de temas individuales en un taller colectivo.</li>
+          <li>Conocimiento y manejo de textos básicos sobre el tema.</li>
+          <li>Análisis de estructuras informativas y narrativas.</li>
+          <li>Empleo de las herramientas técnicas y teóricas proporcionadas.</li>
+          <li>Análisis en pantalla de diversos tipos de guiones y estructuras de cine documental.</li>
+        </ul>
+
+    <p><strong>METODOLOGÍA<p></strong>
+
+        <p>Los participantes trabajarán en proyectos basados en temas propios de tal manera que de cada tema se configure un proyecto diferente a los demás, aunque se parta de una misma base.</p>
+
+        <p>Puesto que un objetivo es desarrollar las habilidades de observación, descripción, asociación, selección, abstracción, síntesis y exposición, el trabajo en aula es una constante. Para ello se recurre a una metodología de análisis de fragmentos de cine documental a partir de los elementos proporcionados en clase y en las lecturas.</p>
+
+        <p>Otra característica fundamental es la presentación de temas que serán objeto de desarrollo a lo largo del curso, en un formato de taller que se basa en la discusión colectiva de las ideas, temas, proyectos, siempre en la perspectiva de aportar para la mejora.</p>
+
+        <p>Así, de forma dinámica, se presentan los temas, por una parte, y, por otra, se conocen conceptos y técnicas que se emplean en el desarrollo de los temas, con el fin de hacer más práctica y perdurable la incorporación de herramientas y conceptos.</p>
+
+        <p>De forma paralela se maneja un proceso de guión hipotético que funciona como testigo para efecto de ejemplificar el proceso completo.</p>
+
+        <p>Se manejan una bibliografía y una filmografía básicas y se hacen visionamientos guiados de películas documentales seleccionadas de acuerdo a los temas de los proyectos. Asimismo se cuenta con un blog que concentre documentos, textos, enlaces con sitios de interés y comentarios de los talleristas.</p>
+
+        <p align="right"><i>El proceso más importante a partir del momento en que se cuenta con un concepto original y hasta que la película es terminada; esto es, la organización de las ideas.</p></i>
+        <p align="right">Russell Porter</p>
+
+        <p>El curso se desarrolla en 12 sesiones de 3 horas cada una y se compone de tres unidades.</p>
+
+    <ol>
+      <li>Introducción al guión para cine documental, taller, objetivos y procedimientos. 1 sesión, 3 horas</li>
+      <li>Construcción de un guión para cine documental. Del tema a la idea, del idea al proyecto, del proyecto al producto. 10 sesiones, 30 horas</li>
+        <ul>
+          <li>a. La preproducción</li>
+          <li>b. Tema - planteamiento de temas personales y tema testigo</li>
+          <li>c. Profundización del tema - ideas de investigación</li>
+          <li>d. Sinopsis</li>
+          <li>e. Escaleta por bloques y secuencias</li>
+          <li>f. Propuesta general</li>
+          <li>g. Nociones de una carpeta de producción documental</li>
+        </ul>
+
+      <li>Presentación y comentarios de proyectos desarrollados. 1 sesión, 3 horas</li>
+    </ol>
+    <br>
+
+   <p><strong>BIBLIOGRAFÍA BÁSICA</strong></p>
+
+    <p>BORDWELL, DAVID Y KRISTIN THOMPSON. Arte cinematográfico. 2002</p>
+    <p>DAS, TRISHA. How to write a documentary script. UNESCO. En línea. S/F</p>
+    <p>FELDMAN, SIMÓN. Guión argumental. Guión documental. Serie Práctica. Gedisa. 1990.</p>
+    <p>GUZMÁN, PATRICIO. El guión en el cine documental. En línea S/F</p>
+    <p>MAZA, MAXIMILIANO Y CRISTINA CERVANTES DE COLLADO. Guión para medios audiovisuales:  cine, radio y televisión. Addison-Wesley Logman, 1994</p>
+    <p>RABIGER, MICHAEL. Dirección de documentales. Instituto Oficial de Radio y Televisión. 2005</p>
+    <p>- - - - - - - - - - - . "Cómo preparar un proyecto documental. Documental y autoría". En línea.</p>
+
+    <p><strong>BIBLIOGRAFÍA COMPLEMENTARIA</p></strong>
+
+    <p>AUFDERHEIDE, PATRICIA. Documentary film. A very short introduction. Oxford University Press. En línea. 2007</p>
+    <p>BARNOUW, ERIC. El documental: historia y estilos. GEDISA. 1996</p>
+    <p>HUNTER, MARK LEE. La investigación a partir de historias: manual para periodistas de investigación. UNESCO. 2013</p>
+    <p>MARQUES, PERE. Introducción al lenguaje audiovisual. En línea. 2010</p>
+    <p>MENDOZA, CARLOS. Guión para cine documental. UNAM. 2012</p>
+    <p>REMEDI, CLAUDIO. El guión documental. En línea. S/F</p>
+
+ 
+    <br><br>
+
+    
+      <div class="col-xs-12 col-sm-12">
+         
+
+          <h3>CONSULTA OTROS CURSOS</h3>
+
+            <div class="row">
+
+            <div class="col-6 col-sm-6 col-lg-6">
+              <h5>SEMINARIO CINE SIN OJO. DEL CINEMATÓGRAFO A LA IMAGEN 3D</h5>
+              <img src="/imgs/cursos/seminario-cine-sin-ojo.jpg" class="img-responsive" 'Seminario cine sin ojo. Del cinematógrafo a la imagen 3D'></a>
+              <p>El seminario busca trazar una arqueología sobre la cuestión técnica e ideológica en el desarrollo de los medios de reproducción como el cine y la cámara fotográfica desde sus inicios.
+              </p>
+              <p>Impartido por: Mtro. Lauro López Sánchez</p>
+              <p><strong>Del 5 de junio al 18 de septiembre de 2015</strong></p>
+              <p>Viernes de 17:00 a 20:00 hrs.</p>
+              <p>$2,000.00</p>
+              <p><a class="btn btn-default" href="/pages/cursos/cine-sin-ojo" role="button">Ver temario »</a></p>
+            </div><!--/span-->
+            </div>
+
+                <div class="col-xs-12 col-sm-12">
+       <h3 class="text-uppercase">Cursos anteriores</h3>
+          <div class="row">
+
+            <div class="col-6 col-sm-6 col-lg-6">
+              <h5>SEMINARIO ESTÉTICA CINEMATOGRÁFICA CINE-MÁQUINA(S)</h5>
+              <img src="/imgs/cursos/curso-cine-maquina.jpg" class="img-responsive" 'Seminario de estética cinematográfica cine máquina'></a>
+              <p>El seminario pretende explorar las ideas-cine de Godard, Kluge, Tarr, Cronenberg, Herzog, Lynch y otros, conectando y tendiendo puentes  con algunos conceptos e ideas de la filosofía contemporánea...
+              </p>
+              <p>Impartido por: Dra. Sonia Rangel Espinosa</p>
+              <p><strong>Del 2 de febrero al 27 de abril de 2015</strong></p>
+              <p>Lunes de 17:00 a 20:00 hrs.</p>
+              <p><a class="btn btn-default" href="/pages/cursos/cine-maquina" role="button">Ver temario »</a></p>
+            </div><!--/span-->
+
+            <div class="col-6 col-sm-6 col-lg-6">
+              <h5>INTRODUCCION A LA HISTORIA DEL CINE MUNDIAL</h5>
+              <img src="/imgs/cursos/cine-mundial.jpg" class="img-responsive" 'Introducción al historia del cine mundial'></a>
+              <p>Programa en XII sesiones de 3 horas cada una, sobre los orígenes, corrientes, géneros, escuelas, exponentes y obras trascendentes que inducen al estudio del cine.</p>
+              <p>Impartido por: Profesor y periodista César Aguilera</p>
+              <p><strong>Inicia el 24 de febrero de 2015</strong></p>
+              <p>Martes de 17:00 a 20:00 hrs.</p>
+              <p><a class="btn btn-default" href="/pages/cursos/introduccion-historia-cine-mundial" role="button">Ver temario »</a></p>
+            </div><!--/span-->
+
+            <div class="col-6 col-sm-6 col-lg-6">
+              <h5>LA MIRADA SINIESTRA: EL CINE SEGÚN  HITCHCOCK (un acercamiento vouyerista)</h5>
+              <img src="/imgs/cursos/mirada-siniestra.jpg" class="img-responsive" 'La mirada siniestra: el cine según Hitchcock'></a>
+              <p>Revisión histórica del legado e influencia de Alfred Hitchcock a través del análisis cinematográfico de sus más importantes obras,  y como  su particular visión del mundo, modificó para siempre el suspenso, el misterio, el terror, así como otros géneros cinematográficos. </p>
+              <p>Impartido por: Juan Santiago Huerta Navarro</p>
+              <p><strong>Del 27 de febrero al 22 de mayo de 2015</strong></p>
+              <p>Viernes de 17:00 a 20:00 hrs.</p>
+              <p><a class="btn btn-default" href="/pages/cursos/mirada-siniestra" role="button">Ver temario »</a></p>
+            </div><!--/span-->
+            
+            <div class="col-6 col-sm-6 col-lg-6">
+              <h5>SEMINARIO ORSON WELLES, MÁS ALLÁ DEL CIUDADANO KANE</h5>
+              <img src="/imgs/cursos/orson-welles.jpg" class="img-responsive" 'Seminario Orson Welles, más allá del Ciudadano Kane'></a>
+              <p>Analizar, a través de 16 sesiones parte de la obra de quien es considerado uno de los artistas más versátiles del siglo XX en el campo del teatro, la radio y el cine, en los que tuvo excelentes resultados.
+             </p>
+              <p>Impartido por: Francisco Ohem</p>
+              <p><strong>Del 11 de marzo al 1 de junio de 2015</strong></p>
+              <p>Miércoles de 17:00 a 20:00 hrs.</p>
+              <p><a class="btn btn-default" href="/pages/cursos/seminario-orson-welles" role="button">Ver temario »</a></p>
+            </div><!--/span-->
+
+          </div><!--/row-->
+
+        </div><!--/span-->
+        </div>
+
+@stop
