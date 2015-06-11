fb81f0be79e3d761b415a2ba9fe76fab361b18e4
monyc2000 <monica.guzman.r@gmail.com>
2015-06-10 19:45:54 -0500

    se crearon paginas que estaban faltantes y se actualizaron links e informacion

---
+++ app/views/pages/Aniversario/index.blade.php
@@ -0,0 +1,18 @@
+@section('breadcrumbs')
+	<li>
+		<a href="/pages/aniversario/index">
+			55 Aniversario
+		</a>
+	</li>
+	
+@stop
+
+
+
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
---
+++ app/views/pages/home/creditos.blade.php
@@ -0,0 +1,18 @@
+@section('breadcrumbs')
+	<li>
+		<a href="/pages/home/creditos">
+			Creditos
+		</a>
+	</li>
+	
+@stop
+
+
+
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
