beb373c41358ce32064eb715666a512a1dd275e1
monyc2000 <monica.guzman.r@gmail.com>
2015-06-30 20:52:02 -0500

    se modificó la ruta de breadcrumbs y se quitó el contenido de la pagina publicaciones de la seccion difusion

--- app/views/pages/difusion/publicaciones.blade.php
+++ app/views/pages/difusion/publicaciones.blade.php
@@ -17,11 +17,7 @@
 
 @section('content')
 
-	<div class="img-servicios">
-		<img src="/assets/imgs/exposiciones-museo.jpg" aling="center">
-	</div>  
-	<h1>Publicaciones</h1>
-
+	{{ $page->body }}
 	
 @stop
 
