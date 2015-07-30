5abc19b67b3f08f206b90edf001ac40b8c121153
monyc2000 <monica.guzman.r@gmail.com>
2015-07-30 17:34:05 -0500

    Se crearon páginas nuevas para la sección aniversario y cursos

---
+++ app/views/pages/cursos/curso-musica-cinematografica.blade.php
@@ -0,0 +1,28 @@
+@section('breadcrumbs')
+<li>
+  <a href="/pages/cursos/cursos-y-talleres">
+    Extensión Academica
+  </a>
+</li>
+<li>
+<a href="/pages/cursos/cursos-y-talleres">
+    Cursos y talleres
+  </a>
+</li>
+<li class="active">
+  Seminario: Filosofía del cine
+</li>
+@stop
+
+
+@section('sidebar')
+  @include('elements.menus.extension-academica', array('selected' => 0))
+@stop
+
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
