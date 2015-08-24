19139cae1176e67077b7527bffdbfebbc5f671ca
monyc2000 <monica.guzman.r@gmail.com>
2015-08-24 18:03:04 -0500

    Se agregaron páginas para las secciones de aniversario y cursos

---
+++ app/views/pages/programa-leer-cine/programa-leer-cine.blade.php
@@ -0,0 +1,23 @@
+@section('breadcrumbs')
+<li>
+  <a href="/pages/cursos/cursos-y-talleres">
+    Extensión Academica
+  </a>
+</li>
+<li class="active">
+Programa leer cine
+</li>
+@stop
+
+
+@section('sidebar')
+  @include('elements.menus.extension-academica', array('selected' => 0))
+@stop
+
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
