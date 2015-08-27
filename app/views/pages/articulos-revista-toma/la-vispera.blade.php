271763446cc42f3cb2e6c8b923f8baac4c73a14e
anahi <chicacalutron@gmail.com>
2015-08-27 00:29:20 -0500

    Se crearon páginas para la seccion de articulos-revista-toma

---
+++ app/views/pages/articulos-revista-toma/la-vispera.blade.php
@@ -0,0 +1,27 @@
+section('breadcrumbs')
+<li>
+	<a href="/press_register">
+		Difusión
+	</a>
+</li>
+<li>
+	<a href="/pages/difusion/articulos">
+		Artículos
+	</a>
+</li>
+<li class="active">
+	La víspera 
+</li>
+
+@stop
+
+@section('sidebar')
+	@include('elements.menus.difusion', array('selected' => 2))
+@stop
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
