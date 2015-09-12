6e97e863723ee44a015069a9349fbad7ac3f80a1
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-09-12 14:18:24 -0500

    Reparaciones de rutas y separación de api.

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -6,8 +6,8 @@
 |----------------------------------------------------------------------------
  */
 
-Route::group(['prefix' => 'admin', 'before' => 'validate_admin'], function()
-{
+Route::group(['prefix' => 'admin', 'before' => 'validate_admin'], function () {
+
     /*
     |--------------------------------------------------------------------------
     | DASHBOARD
@@ -16,84 +16,16 @@ Route::group(['prefix' => 'admin', 'before' => 'validate_admin'], function()
 
     Route::get('/', [
         'as' => 'admin.dashboard',
-        'uses' => function(){
+        'uses' => function () {
             Redirect::to('/admin/dashboard/login');
         }]);
-
-    /*
-    |--------------------------------------------------------------------------
-    | API.
-    |--------------------------------------------------------------------------
-     */
-
-    Route::group(['prefix' => 'api'], function () {
-
-        /*
-         |--------------------------------------------------------------------------
-         | FILMS
-         |--------------------------------------------------------------------------
-         */
-
-        Route::group(['prefix' => 'films'], function () {
-
-            Route::get('/', 'Api\FilmController@index');
-            Route::post('/', 'Api\FilmController@store');
-            Route::get('/{id}', 'Api\FilmController@show');
-            Route::put('/{id}', 'Api\FilmController@update');
-            Route::delete('/{id}', 'Api\FilmController@destroy');
-        });
-
-        View::name('layouts.modal', 'modal');
-
-        Route::get('film/create', function()
-        {
-            return View::make('api.films.create');
-        });
-
-        Route::get('iconographic/create', function()
-        {
-            return View::make('api.iconographics.create');
-        });
-
-        Route::post('iconographic/store', [
-            'as' => 'admin.api.iconographic.store',
-            'uses' => 'Api\IconographicController@store']);
-
-        Route::group(['prefix' => 'iconographic'], function()
-        {
-            Route::post('/{id}',     'Api\IconographicController@update');
-//            The PUT verb not work. Someone said to is blame of CSRF protection.
-//            Route::put('/{id}',     'Api\IconographicController@update');
-            Route::delete('/{id}',  'Api\IconographicController@destroy');
-        });
-
-        /*
-        |--------------------------------------------------------------------------
-        | Exhibitions
-        |--------------------------------------------------------------------------
-         */
-
-        Route::group(['prefix' => 'exhibition'], function()
-        {
-            Route::get('', 		'Api\ExhibitionController@index');
-            Route::post('',		'Api\ExhibitionController@store');
-            Route::put('/{id}', 	'Api\ExhibitionController@update');
-            Route::delete('/{id}', 	'Api\ExhibitionController@destroy');
-
-            Route::post('/{exhibition_id}/schedule', 	'Api\ScheduleController@store');
-        });
-
-        Route::put('schedule/{id}', 	'Api\ScheduleController@update');
-        Route::delete('schedule/{id}', 	'Api\ScheduleController@destroy');
-    });
-
 
     /*
     |--------------------------------------------------------------------------
     | Aplicación de cursos (ADMIN)
     |--------------------------------------------------------------------------
      */
-    Route::get('course/app', function(){
+    Route::get('course/app', function () {
 
         return View::make('courses.admin-app');
     });
@@ -105,7 +37,7 @@ Route::group(['prefix' => 'admin', 'before' => 'validate_admin'], function()
      */
     Route::get('/exhibition/app', [
         'as' => 'admin.exhibition.app',
-        'uses' => function(){
+        'uses' => function () {
             return View::make('exhibitions.app');
         }
     ]);
@@ -127,22 +59,22 @@ Route::group(['prefix' => 'admin', 'before' => 'validate_admin'], function()
     /**
      * El nombre de las rutas tienen el prefijo admin. (incluyendo el punto)
      */
-    array_map(function($resource)
-    {
-        Route::resource($resource, sprintf('Resources\%sController', ucfirst($resource) ) );
-    }, $resources );
+    array_map(function ($resource) {
 
-    Route::get('billboard/send',[
+        Route::resource($resource, sprintf('Resources\%sController', ucfirst($resource)));
+    }, $resources);
+
+    Route::get('billboard/send', [
         'as'=> 'admin.billboard.send',
         'uses' => 'Resources\BillboardConstroller@Send']);
 
-    Route::get('/press_register/index',
-        array(
+    Route::get('/press_register/index', [
             'as' => 'admin.press_register.index',
-            'uses' => 'PressRegisterController@index' ));
+            'uses' => 'PressRegisterController@index'
+    ]);
 
-    Route::delete('/press_register/destroy/{id}',
-        array(
+    Route::delete('/press_register/destroy/{id}', [
             'as' => 'admin.press_register.destroy',
-            'uses' => 'PressRegisterController@destroy' ));
+            'uses' => 'PressRegisterController@destroy'
+    ]);
 });
--- app/routes/frontend.php
+++ app/routes/frontend.php
@@ -1,37 +1,34 @@
 <?php
 
-Route::get('/home',[
+Route::get('/home', [
     'as' => 'home',
-    'uses' => function(){
+    'uses' => function () {
         return Redirect::to('/');
     }]);
 
 Route::get('/', 'HomeController@index');
 
-/**
- * Exhibitiones frontend
- */
-Route::group(['prefix' => 'exhibition'], function(){
+Route::group(['prefix' => 'exhibition'], function () {
 
     // app
-    Route::get('',				'ExhibitionController@index');
-    Route::get('history',		'ExhibitionController@history');
-    Route::get('find',[
-        'as'	=> 'exhibition.find',
-        'uses' 	=> 'ExhibitionController@find'
+    Route::get('', 'ExhibitionController@index');
+    Route::get('history', 'ExhibitionController@history');
+    Route::get('find', [
+        'as' => 'exhibition.find',
+        'uses' => 'ExhibitionController@find'
     ]);
 
-    Route::get('{id}/show',[
-        'as' 	=> 'exhibition.show',
-        'uses' 	=> 'ExhibitionController@detail'
+    Route::get('{id}/show', [
+        'as' => 'exhibition.show',
+        'uses' => 'ExhibitionController@detail'
     ]);
-    Route::get('{id}/detail-history',[
-        'as'	=> 'exhibition.detail-history',
-        'uses' 	=> 'ExhibitionController@detailHistory'
+    Route::get('{id}/detail-history', [
+        'as' => 'exhibition.detail-history',
+        'uses' => 'ExhibitionController@detailHistory'
     ]);
-    Route::get('{id}/detail-home',[
-        'as' 	=> 'exhibition.detail-home',
-        'uses' 	=> 'ExhibitionController@detailHome'
+    Route::get('{id}/detail-home', [
+        'as' => 'exhibition.detail-home',
+        'uses' => 'ExhibitionController@detailHome'
     ]);
 
     Route::get('/filter/{name?}/{id?}/{title?}', 'ExhibitionController@index');
@@ -44,34 +41,32 @@ Route::get('/news/show/{id}', 'NewsController@show');
 Route::get('/news/index', 'NewsController@index');
 
 
-Route::get('/press_register',
-    array(
+Route::get('/press_register', [
         'as' => 'press_register.create',
-        'uses' => 'PressRegisterController@create' ));
+        'uses' => 'PressRegisterController@create'
+]);
 
-Route::post('/press_register/store',
-    array(
+Route::post('/press_register/store', [
         'as' => 'press_register.store',
-        'uses' => 'PressRegisterController@store' ));
+        'uses' => 'PressRegisterController@store'
+]);
 
-Route::get('/filmoteca-medal/',
-    array(
+Route::get('/filmoteca-medal/', [
         'as' => 'filmoteca-medal/',
-        'uses' => 'FilmotecaMedalController@index')
-);
+        'uses' => 'FilmotecaMedalController@index'
+]);
 
-Route::get('/chronology/',
-    array(
+Route::get('/chronology/', [
         'as' => 'chronology',
-        'uses' => 'ChronologyController@index')
-);
+        'uses' => 'ChronologyController@index'
+]);
 
 Route::get('/billboard', [
     'as' => 'billboard',
     'uses' => 'BillboardController@index'
 ]);
 
-Route::get('/pages/quienes-somos/cronologia',function(){
+Route::get('/pages/quienes-somos/cronologia', function () {
 
     return Redirect::route('chronology');
 });
@@ -82,7 +77,7 @@ Route::get('/pages/quienes-somos/cronologia',function(){
 |------------------------------------------------------------------------------
  */
 
-Route::get('/courses/app', function(){
+Route::get('/courses/app', function () {
 
     return View::make('courses.app');
 });
@@ -100,85 +95,4 @@ Route::get('/auditorim/show/{id}', 'AuditoriumController@show');
 |----------------------------------------------------------------------------
  */
 
-Route::get('/pages/{dir_or_name}/{name?}','PageController@show');
-
-/*
-|----------------------------------------------------------------------------
-| API ROUTES. Rutas para llamadas ajax publicas.
-|----------------------------------------------------------------------------
- */
-
-Route::group(['prefix' => 'api'], function () {
-
-    Route::get('genres', 'Api\GenreController@index');
-    Route::get('countries', 'Api\CountryController@index');
-
-    /**
-     * Establecemos el layout. Esto es únicamente para las rutas.
-     * Los controladores definen su propio layout.
-     */
-    View::name('layouts.modal', 'modal');
-
-    Route::get('film/search', [
-        'as' => 'api.film.search',
-        'uses' => 'Api\FilmController@search']);
-
-    Route::get('film','Api\FilmController@search');
-
-    Route::get('auditorium/all', [
-        'as' => 'api.auditorium.all',
-        'uses' => 'Api\AuditoriumController@all']);
-
-    Route::get('iconographic/all', [
-        'as' => 'api.iconographic.all',
-        'uses' => 'Api\IconographicController@all']);
-
-    Route::get('auditorium/{id}/detail',[
-        'as' => 'api.auditorium.detail',
-        'uses' => 'Api\AuditoriumController@detail']);
-
-    Route::group(['prefix' => 'exhibition'], function(){
-
-        Route::get('/{id}', 'Api\ExhibitionController@show');
-    });
-
-    /*
-    |------------------------------------------------------------------------------
-    | CURSOS
-    |------------------------------------------------------------------------------
-     */
-
-    Route::post('courses/student/signup',[
-        'as' => 'api.courses.student.signup',
-        'uses' => 'Api\Courses\StudentController@signup']);
-
-    Route::post('courses/student/login',[
-        'as' => 'api.courses.student.login',
-        'uses' => 'Api\Courses\StudentController@login']);
-
-    Route::get('courses/student/logout',[
-        'as' => 'api.courses.student.logout',
-        'uses' => 'Api\Courses\StudentController@logout']);
-
-    Route::get('courses/student/recover-password', [
-        'as' => 'api.courses.student.recover-password',
-        'uses' => 'Api\Courses\StudentController@recoverPassword']);
-
-    Route::post('courses/student/change-password', [
-        'as' => 'api.courses.student.change-password',
-        'uses' => 'Api\Courses\StudentController@changePassword']);
-
-    Route::post('courses/student/update', [
-        'as' => 'api.courses.student.update',
-        'uses' => 'Api\Courses\StudentController@update']);
-
-    Route::get('courses/course/{id}/signup',[
-        'as' => 'api.courses.course',
-        'uses' => 'Api\Courses\CourseController@signup']);
-
-    Route::get('courses/student/courses','Api\Courses\StudentController@courses');
-
-    Route::get('courses/course', 'Api\Courses\CourseController@index');
-
-    Route::get('courses/course/{id}','Api\Courses\CourseController@show');
-});
+Route::get('/pages/{dir_or_name}/{name?}', 'PageController@show');
