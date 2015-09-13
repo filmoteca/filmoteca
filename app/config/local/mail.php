743b77d60b911d6707057a33206ca14195e8d595
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-09-13 16:24:40 -0500

    Configuración local de email.
    [#156]

--- app/config/local/mail.php
+++ app/config/local/mail.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-	'driver' => 'log'
+    'driver' => 'log',
+    'pretend' => false,
+    'from' => [
+        'address' => 'filmoteca@unam.mx',
+        'name' => 'Filmoteca'
+    ],
+    'error' => [
+        'address' => 'mail_address_to_notify_some_error@unam.mx',
+        'subject' => 'Error Notification'
+    ]
 ];
