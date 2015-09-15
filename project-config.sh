70712bb98cbe4a922f1c86d78a3df12d172db4fb
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-09-14 20:30:49 -0500

    HTML minification waw activated in production.

--- project-config.sh
+++ project-config.sh
@@ -8,7 +8,8 @@ bower install
 
 php artisan migrate --env=local
 php artisan migrate --package=cartalyst/sentry
-php artisan syntara:install --env=local
+php artisan asset:publish mrjuliuss/syntara
+php artisan migrate --package="mrjuliuss/syntara"
 php artisan db:seed --env=local
 php artisan asset:publish frozennode/administrator --env=local
 php artisan create:user filmoteca filmoteca@unam.mx filmoteca Admin --env=local
