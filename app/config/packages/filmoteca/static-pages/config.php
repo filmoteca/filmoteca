256e33a6c80101993021e3f8dd740f2f23d9dbc0
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-12-13 20:51:26 -0600

    Integrate static page packages.

--- app/config/packages/filmoteca/static-pages/config.php
+++ app/config/packages/filmoteca/static-pages/config.php
@@ -6,12 +6,15 @@ return [
      | Not add slash neither at the first and at the end, because the slashes are going to replaced with a dot
      | to the named route.
      */
-    'admin-url'     => 'filmoteca/static-pages',
-    'pages-layout'  => 'filmoteca/static-pages::layouts.pages',
+    'admin-url-prefix'      => 'admin/static-pages',
+    'pages-url-prefix'     => 'pages',
+    'auth-filter'           => 'validate_admin',
+    'pages-layout'  => 'layouts/default',
     'sections'      => [
         'title'     => 'title',
         'sidebar'   => 'sidebar',
         'main-menu' => 'main-menu',
         'content'   => 'content'
-    ]
+    ],
+    'main-menu-name' => 'main'
 ];
