c5617668e2fd5f3d3a0261131de058727f7e785d
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-12-13 20:50:06 -0600

    Usando PRS2.

--- app/filters.php
+++ app/filters.php
@@ -11,15 +11,13 @@
 |
 */
 
-App::before(function($request)
-{
-	//
+App::before(function ($request) {
+    //
 });
 
 
-App::after(function($request, $response)
-{
-	//
+App::after(function ($request, $response) {
+    //
 });
 
 /*
@@ -33,25 +31,19 @@ App::after(function($request, $response)
 |
 */
 
-Route::filter('validate_admin', function()
-{
-	if (!Sentry::check())
-	{
-		if (Request::ajax())
-		{
-			return Response::make('Unauthorized', 401);
-		}
-		else
-		{
-			return Redirect::guest('admin/dashboard/login');
-		}
-	}
+Route::filter('validate_admin', function () {
+    if (!Sentry::check()) {
+        if (Request::ajax()) {
+            return Response::make('Unauthorized', 401);
+        } else {
+            return Redirect::guest('admin/dashboard/login');
+        }
+    }
 });
 
 
-Route::filter('auth.basic', function()
-{
-	return Auth::basic();
+Route::filter('auth.basic', function () {
+    return Auth::basic();
 });
 
 /*
@@ -65,9 +57,10 @@ Route::filter('auth.basic', function()
 |
 */
 
-Route::filter('guest', function()
-{
-	if (Auth::check()) return Redirect::to('/');
+Route::filter('guest', function () {
+    if (Auth::check()) {
+        return Redirect::to('/');
+    }
 });
 
 /*
@@ -81,20 +74,17 @@ Route::filter('guest', function()
 |
 */
 
-Route::filter('csrf', function()
-{
-	if (Session::token() != Input::get('_token'))
-	{
-		throw new Illuminate\Session\TokenMismatchException;
-	}
+Route::filter('csrf', function () {
+    if (Session::token() != Input::get('_token')) {
+        throw new Illuminate\Session\TokenMismatchException;
+    }
 });
 
-View::composer('*', function($view)
-{
+View::composer('*', function ($view) {
     $view->with('siteName', 'Filmoteca UNAM');
 });
 
-View::composer(['layouts.dashboard.master', 'layouts.dashboard.master-app'], function($view){
+View::composer(['layouts.dashboard.master', 'layouts.dashboard.master-app'], function ($view) {
 
-	$view->with('currentUser', Sentry::getUser());
+    $view->with('currentUser', Sentry::getUser());
 });
