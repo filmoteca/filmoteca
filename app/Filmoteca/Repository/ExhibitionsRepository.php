8c37d883febdae9acd8886844c976135c8db9da1
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-12-19 21:27:53 -0600

    Code style

--- app/Filmoteca/Repository/ExhibitionsRepository.php
+++ app/Filmoteca/Repository/ExhibitionsRepository.php
@@ -11,158 +11,149 @@ use Illuminate\Database\Eloquent\Collection;
 
 class ExhibitionsRepository extends ResourcesRepository implements PageableRepositoryInterface
 {
-	public function __construct(
-		Exhibition $exhibition,
-		ExhibitionFilm $exhibitionFilm,
-		Film $film
-	) {
-		$this->exhibition       = $exhibition;
-		$this->exhibitionFilm   = $exhibitionFilm;
-		$this->resource         = $exhibition;
-		$this->film             = $film;
-	}
+    public function __construct(
+        Exhibition $exhibition,
+        ExhibitionFilm $exhibitionFilm,
+        Film $film
+    ) {
+        $this->exhibition = $exhibition;
+        $this->exhibitionFilm = $exhibitionFilm;
+        $this->resource = $exhibition;
+        $this->film = $film;
+    }
 
-	/**
-	 * Realiza una búsqueda de exhibiciones dependiendo
-	 * del primer parámetro.
-	 * @param  String $by    Esta cadena indica el tipo de búsqueda que se realizara.
-	 * @param  Mixed $value El valor con el cual se  quiere que coincida la búsqueda
-	 * y depende el tipo de búsqueda.
-	 * @return Collection 	Collección de exhibiciones
-	 * @throws NotFoundException Si la exhibición no existe al realizar una búsquda por id.
-	 */
-	public function search($by, $value = null)
-	{
-		switch($by)
-		{
-			case('id'):
+    /**
+     * Realiza una búsqueda de exhibiciones dependiendo
+     * del primer parámetro.
+     * @param  String $by Esta cadena indica el tipo de búsqueda que se realizara.
+     * @param  Mixed $value El valor con el cual se  quiere que coincida la búsqueda
+     * y depende el tipo de búsqueda.
+     * @return Collection    Collección de exhibiciones
+     * @throws NotFoundException Si la exhibición no existe al realizar una búsquda por id.
+     */
+    public function search($by, $value = null)
+    {
+        switch ($by) {
+            case ('id'):
+                $exhibitions = $this->exhibition->findOrFail($value);
+                break;
+            case ('title'):
+                $exhibitions = $this->searchByTitle($value);
+                break;
+            case ('date'):
+                $exhibitions = $this->searchByDate($value[0], $value[1]);
+                break;
+            case ('today'):
+                $today = Carbon::now()->toDateString();
+                $exhibitions = $this->searchByDate($today, $today . ' 23:59:59');
+                break;
+            default:
+                throw new InvalidArgumentException('Parámetro de búsqueda invalido: ' . $by);
+        }
 
-				$exhibitions = $this->exhibition->findOrFail($value);
-				
-				break;
-			case('title'):
+        return $exhibitions;
+    }
 
-				$exhibitions = $this->searchByTitle( $value );
+    /**
+     * Realiza una búsquda de aquellas exhibiciones que
+     * exhiban una película con el título dado.
+     * @param  [type] $title [description]
+     * @return [type]        [description]
+     */
+    public function searchByTitle($title)
+    {
+        $films = $this->film
+            ->where('title', 'like', '%' . $value . '%')
+            ->get(array('id'));
 
-				break;
-			case('date'):
+        /**
+         * El id de ExhibitionFilm es igual (o tendría que serlo) al
+         * de Films ya que la relación es uno a uno.
+         */
 
-				$exhibitions = $this->searchByDate($value[0], $value[1]);
-				break;
-			case('today'):
+        $exhibitions = $this->exhibition
+            ->whereIn('id', $films->lists('id'), 'or')
+            ->with('exhibitionFilm', 'exhibitionFilm.film')
+            ->get();
 
-				$today = Carbon::now()->toDateString();
-				$exhibitions = $this->searchByDate( $today, $today . ' 23:59:59');
+        /**
+         * Cargamos la programación de las exhibiciones encontradas.
+         * La sentencia dentro del foreach lo que hace es inicializar
+         * el atributo schedules de una exhibición.
+         */
+        foreach ($exhibitions as $exhibition) {
+            $exhibition->schedules;
+        }
 
-				break;
-			default:
-				throw new InvalidArgumentException('Parámetro de búsqueda invalido: ' . $by );
-		}
+        return $exhibitions;
+    }
 
-		return $exhibitions;
-	}
+    /**
+     * Busca todas las exhibiciones que esten entre dos fechas
+     * incluyendo aquellas de la fecha incial y final.
+     * @param  String $from Fecha de inicio.
+     * @param  String $until Fecha de fin.
+     * @return Collection        Colección de exhibiciones.
+     */
+    public function searchByDate($from, $until)
+    {
+        $interval = array($from, $until . ' 23:59:59');
 
-	/**
-	 * Realiza una búsquda de aquellas exhibiciones que
-	 * exhiban una película con el título dado.
-	 * @param  [type] $title [description]
-	 * @return [type]        [description]
-	 */
-	public function searchByTitle($title)
-	{
-		$films = $this->film
-					->where('title','like', '%'. $value . '%')
-					->get(array('id'));
-
-		/**
-		 * El id de ExhibitionFilm es igual (o tendría que serlo) al
-		 * de Films ya que la relación es uno a uno.
-		 */
-
-		$exhibitions = $this->exhibition
-					->whereIn('id', $films->lists('id'), 'or')
-					->with('exhibitionFilm','exhibitionFilm.film')
-					->get();
-
-		/**
-		 * Cargamos la programación de las exhibiciones encontradas.
-		 * La sentencia dentro del foreach lo que hace es inicializar
-		 * el atributo schedules de una exhibición.
-		 */
-		foreach($exhibitions as $exhibition )
-		{
-			$exhibition->schedules;
-		}
-
-		return $exhibitions;
-	}
-
-	/**
-	 * Busca todas las exhibiciones que esten entre dos fechas
-	 * incluyendo aquellas de la fecha incial y final.
-	 * @param  String $from  Fecha de inicio.
-	 * @param  String $until Fecha de fin. 
-	 * @return Collection        Colección de exhibiciones.
-	 */
-	public function searchByDate($from, $until)
-	{
-		$interval = array($from , $until . ' 23:59:59');
-
-		$exhibitions = $this->exhibition
-			->whereHas('schedules', function($query) use ($interval)
-			{
-				$query->whereBetween('entry', $interval);
-			})->with(
+        $exhibitions = $this->exhibition
+            ->whereHas('schedules', function ($query) use ($interval) {
+                $query->whereBetween('entry', $interval);
+            })->with(
                 'schedules',
                 'schedules.auditorium',
-				'exhibitionFilm',
-				'exhibitionFilm.film',
+                'exhibitionFilm',
+                'exhibitionFilm.film',
                 'exhibitionFilm.film.genre',
                 'exhibitionFilm.film.countries',
-				'type'
+                'type'
             )->get();
 
-		return $exhibitions;
-	}
+        return $exhibitions;
+    }
 
-	/**
-	 * El código de esta función quedo muy mal. Yo esperabá que eloquente
-	 * pudiera guardar los datos que en el array $data sin problemas.
-	 * pero te tengo que acomodar cada dato para que coincida con las columnas
-	 * de la base de datos.
-	 *
-	 * Hay que encontrar una mejor forma de hacer esto.
-	 *
-	 * ## ENHANCEMENT
-	 * @param  [type] $data [description]
-	 * @return [type]       [description]
-	 */
-	public function store(array $data = null)
-	{
-		$d = [];
-		$d['type_id'] = $data['type']['id'];
+    /**
+     * El código de esta función quedo muy mal. Yo esperabá que eloquente
+     * pudiera guardar los datos que en el array $data sin problemas.
+     * pero te tengo que acomodar cada dato para que coincida con las columnas
+     * de la base de datos.
+     *
+     * Hay que encontrar una mejor forma de hacer esto.
+     *
+     * ## ENHANCEMENT
+     * @param  [type] $data [description]
+     * @return [type]       [description]
+     */
+    public function store(array $data = null)
+    {
+        $d = [];
+        $d['type_id'] = $data['type']['id'];
 
-		$exhibitionFilm = App::make('Filmoteca\Models\Exhibitions\ExhibitionFilm');
+        $exhibitionFilm = App::make('Filmoteca\Models\Exhibitions\ExhibitionFilm');
 
-		$exhibitionFilm->fill(['film_id' => $data['exhibition_film']['film']['id'] ])
-			->save();
+        $exhibitionFilm->fill(['film_id' => $data['exhibition_film']['film']['id']])
+            ->save();
 
-		$d['exhibition_film_id'] = $exhibitionFilm->id;
+        $d['exhibition_film_id'] = $exhibitionFilm->id;
 
-		return $this->resource->create($d);
-	}
+        return $this->resource->create($d);
+    }
 
-	public function find($id)
-	{
-		return $this->resource->where('id', $id)
-			->with(
-				'schedules', 
-				'schedules.auditorium',
-				'exhibitionFilm',
-				'exhibitionFilm.film',
-				'type')
-			->first();
-	}
+    public function find($id)
+    {
+        return $this->resource->where('id', $id)
+            ->with(
+                'schedules',
+                'schedules.auditorium',
+                'exhibitionFilm',
+                'exhibitionFilm.film',
+                'type'
+            )
+            ->first();
+    }
 
     /**
      * @param int $page
@@ -170,154 +161,154 @@ class ExhibitionsRepository extends ResourcesRepository implements PageableRepos
      * @param int $amount
      * @return \Filmoteca\Pagination\Results
      */
-	public function paginate($page = 1, $query = '', $amount = 15)
-	{
-		$results = Results::make();
+    public function paginate($page = 1, $query = '', $amount = 15)
+    {
+        $results = Results::make();
 
-		$resources = $this
-			->resource
-            ->whereHas('exhibitionFilm', function($q) use ($query){
+        $resources = $this
+            ->resource
+            ->whereHas('exhibitionFilm', function ($q) use ($query) {
 
-                $q->whereHas('film', function($q) use ($query) {
+                $q->whereHas('film', function ($q) use ($query) {
                     $q->where('films.title', 'like', '%' . $query . '%');
                 });
             })
-			->orderBy('id','desc')
-			->skip($amount * ($page - 1))
-			->take($amount)
-			->with('exhibitionFilm','exhibitionFilm.film')
-			->get();
+            ->orderBy('id', 'desc')
+            ->skip($amount * ($page - 1))
+            ->take($amount)
+            ->with('exhibitionFilm', 'exhibitionFilm.film')
+            ->get();
 
         $total = $this
             ->resource
-            ->whereHas('exhibitionFilm', function($q) use ($query){
+            ->whereHas('exhibitionFilm', function ($q) use ($query) {
 
-                $q->whereHas('film', function($q) use ($query) {
+                $q->whereHas('film', function ($q) use ($query) {
                     $q->where('films.title', 'like', '%' . $query . '%');
                 });
             })
             ->count();
 
-		$results->setTotal($total);
-		$results->setItems($resources);
+        $results->setTotal($total);
+        $results->setItems($resources);
 
-		return $results;
-	}
+        return $results;
+    }
 
-	public function update($id, array $data = null)
-	{
-		$exhibition = $this->resource
-			->findOrFail($data['id'])
-			->fill($data);
+    public function update($id, array $data = null)
+    {
+        $exhibition = $this->resource
+            ->findOrFail($data['id'])
+            ->fill($data);
 
-		$exhibition->save();
+        $exhibition->save();
 
-		return true;
-	}
+        return true;
+    }
 
     public function findByTitleDirector($fields)
     {
-        $builder    = Film::getQuery();
-        $resources  = Collection::make([]);
-        $limit      = 15;
+        $builder = Film::getQuery();
+        $resources = Collection::make([]);
+        $limit = 15;
 
-        foreach($fields as $name => $value)
-        {
-            $builder->where($name, 'like' ,'%' . $value . '%');
+        foreach ($fields as $name => $value) {
+            $builder->where($name, 'like', '%' . $value . '%');
         }
 
         $results = $builder->get(['id']);
 
-        $filmsIds = array_map(function($dummyObject){
+        $filmsIds = array_map(function ($dummyObject) {
             return $dummyObject->id; //The query builder returns an array and does not a collection. I do not know why.
-        }, $results );
+        }, $results);
 
-        if( !empty($filmsIds) ){
-            $resources = Exhibition::whereHas('exhibitionFilm', function($q) use ($filmsIds){
+        if (!empty($filmsIds)) {
+            $resources = Exhibition::whereHas('exhibitionFilm', function ($q) use ($filmsIds) {
 
-                $q->whereHas('film', function($q) use ($filmsIds) {
+                $q->whereHas('film', function ($q) use ($filmsIds) {
                     $q->whereIn('films.id', $filmsIds);
                 });
             })->with('schedules',
                 'schedules.auditorium',
                 'exhibitionFilm',
                 'exhibitionFilm.film',
-                'type')
+                'type'
+            )
                 ->paginate($limit);
         }
 
         return $resources;
     }
 
-	/**
-	 * The exhibitions of the current month.
-	 *
-	 * @return Collection
-	 */
-	public function findLast()
-	{
-		$today = Carbon::now();
+    /**
+     * The exhibitions of the current month.
+     *
+     * @return Collection
+     */
+    public function findLast()
+    {
+        $today = Carbon::now();
 
-		$from = Carbon::createFromDate(
-			$today->year,
-			$today->month,
-			1
-		)->toDateString();
+        $from = Carbon::createFromDate(
+            $today->year,
+            $today->month,
+            1
+        )->toDateString();
 
-		$until = Carbon::createFromDate(
-			$today->year,
-			$today->month,
-			$today->daysInMonth
-		)->toDateString();
+        $until = Carbon::createFromDate(
+            $today->year,
+            $today->month,
+            $today->daysInMonth
+        )->toDateString();
 
-		return $this->searchByDate($from, $until);
-	}
+        return $this->searchByDate($from, $until);
+    }
 
-	/**
-	 * @param int $month
-	 * @return Collection
-	 */
-	public function findByMonth($month = 0)
-	{
-		if (!is_int($month) || $month < 1 || $month > 12) {
-			return $this->exhibition->findLast();
-		}
+    /**
+     * @param int $month
+     * @return Collection
+     */
+    public function findByMonth($month = 0)
+    {
+        if (!is_int($month) || $month < 1 || $month > 12) {
+            return $this->exhibition->findLast();
+        }
 
-		$currentDate = Carbon::now();
-		$currentDate->month = $month;
+        $currentDate = Carbon::now();
+        $currentDate->month = $month;
 
-		$from = Carbon::createFromDate(
-			$currentDate->year,
-			$currentDate->month,
-			1
-		)->toDateString();
+        $from = Carbon::createFromDate(
+            $currentDate->year,
+            $currentDate->month,
+            1
+        )->toDateString();
 
-		$until = Carbon::createFromDate(
-			$currentDate->year,
-			$currentDate->month,
-			$currentDate->daysInMonth
-		)->toDateString();
+        $until = Carbon::createFromDate(
+            $currentDate->year,
+            $currentDate->month,
+            $currentDate->daysInMonth
+        )->toDateString();
 
-		return $this->searchByDate($from, $until);
-	}
+        return $this->searchByDate($from, $until);
+    }
 
-	protected function makeSchedules(array $schedules = null)
-	{
-		return array_map(function($a_schedule){
+    protected function makeSchedules(array $schedules = null)
+    {
+        return array_map(function ($a_schedule) {
 
-			if( isset($data['id'])) {
-				$schedule = App::make('Filmoteca\Models\Exhibitions\Schedule')
-					->findOrFail($a_schedule['id'])
-					->fill($a_schedule)
-					->save();
-			} else {
-				$a_schedule['auditorium_id'] = $a_schedule['auditorium']['id'];
-				$schedule = App::make('Filmoteca\Models\Exhibitions\Schedule')
-					->fill($a_schedule)
-					->save();
-			}
+            if (isset($data['id'])) {
+                $schedule = App::make('Filmoteca\Models\Exhibitions\Schedule')
+                    ->findOrFail($a_schedule['id'])
+                    ->fill($a_schedule)
+                    ->save();
+            } else {
+                $a_schedule['auditorium_id'] = $a_schedule['auditorium']['id'];
+                $schedule = App::make('Filmoteca\Models\Exhibitions\Schedule')
+                    ->fill($a_schedule)
+                    ->save();
+            }
 
-			return $schedule;
-		}, $schedules);
-	}
+            return $schedule;
+        }, $schedules);
+    }
 }
