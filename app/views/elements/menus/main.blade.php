aa23fa35e46b5151a7ee7792a7684a9fd657f229
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2015-12-28 22:00:45 -0600

    Se agregó validación antes de raterizar el main menu

--- app/views/elements/menus/main.blade.php
+++ app/views/elements/menus/main.blade.php
@@ -4,19 +4,20 @@
         <div class="navbar-header">
 
             <button type="button" class="navbar-toggle" data-toggle="collapse"
-                data-target=".navbar-ex1-collapse">
+                    data-target=".navbar-ex1-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand-logo" href="/pages/aniversario55/index" title="55 Aniversario Filmoteca UNAM"><img src="/assets/imgs/filmo55aniversario.png" alt="logo 55aniversario"></a>
-      </div>
+            <a class="navbar-brand-logo" href="/pages/aniversario55/index" title="55 Aniversario Filmoteca UNAM"><img
+                        src="/assets/imgs/filmo55aniversario.png" alt="logo 55aniversario"></a>
+        </div>
 
         <div class="collapse navbar-collapse navbar-ex1-collapse">
-
-            {{ HTML::menu($mainMenu->getEntries(), 'nav navbar-nav', '','', 'dropdown-menu' ) }}
-
+            @if ($mainMenu !== null)
+                {{ HTML::menu($mainMenu->getEntries(), 'nav navbar-nav', '','', 'dropdown-menu' ) }}
+            @endif
         </div>
     </nav>
 </div>
