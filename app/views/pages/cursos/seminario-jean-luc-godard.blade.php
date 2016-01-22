c468bf9fa56bb7c541a928629c1b4abf28fd0e3b
Monica <monica@MacBook-Pro-de-Monica.local>
2016-01-22 16:14:23 -0600

    Se crearon nuevas páginas para la sección cursos

---
+++ app/views/pages/cursos/seminario-jean-luc-godard.blade.php
@@ -0,0 +1,27 @@
+@section('breadcrumbs')
+<li>
+  <a href="/pages/cursos/cursos-y-talleres">
+    Extensión Academica
+  </a>
+</li>
+<li>
+<a href="/pages/cursos/cursos-y-talleres">
+    Cursos y talleres
+  </a>
+</li>
+<li class="active">
+Seminario Jean Luc Godard: La Imagen - Pensamiento
+</li>
+@stop
+
+
+@section('sidebar')
+  @include('elements.menus.extension-academica', array('selected' => 0))
+@stop
+
+
+@section('content')
+
+    {{ $page->body }}
+
+@stop
