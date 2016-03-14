aedeed66a843d71b47dd5d64ab1d625e4dd2a5f8
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-03-13 19:28:26 -0600

    Added title and subtitle to calendar

--- app/views/frontend/exhibitions/partials/calendar.blade.php
+++ app/views/frontend/exhibitions/partials/calendar.blade.php
@@ -1,42 +1,48 @@
-<table>
-    <tr>
-        <th>
-            @lang('dates.week-days-abbreviations.Su')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.Mo')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.Tu')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.We')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.Th')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.Fr')
-        </th>
-        <th>
-            @lang('dates.week-days-abbreviations.Sa')
-        </th>
-    </tr>
-    @foreach ($calendar->getWeeks() as $week)
+<div class="calendar">
+    <div class="header">
+        <h3>@lang('exhibitions.frontend.calendar.title')</h3>
+        <h4>@lang('exhibitions.frontend.calendar.subtitle')</h4>
+    </div>
+    <table>
         <tr>
-            @foreach($week->getDays() as $day)
-                <td>
-                    @if ($day->getDate()->month === $date->month)
-                        @if ($day->hasExhibitions())
-                            <a class="active" href="{{ $day->getDateInFormatToUrl() }}">{{ $day->getNumber() }}</a>
-                        @else
-                            <a class="active">{{ $day->getNumber() }}</a>
-                        @endif
-                    @else
-                        <span class="disabled">{{ $day->getNumber() }}</span>
-                    @endif
-                </td>
-            @endforeach
+            <th>
+                @lang('dates.week-days-abbreviations.Su')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.Mo')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.Tu')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.We')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.Th')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.Fr')
+            </th>
+            <th>
+                @lang('dates.week-days-abbreviations.Sa')
+            </th>
         </tr>
-    @endforeach
-</table>
+        @foreach ($calendar->getWeeks() as $week)
+            <tr>
+                @foreach($week->getDays() as $day)
+                    <td>
+                        @if ($day->getDate()->month === $date->month)
+                            @if ($day->hasExhibitions())
+                                <a class="active" href="{{ $day->getDateInFormatToUrl() }}">{{ $day->getNumber() }}</a>
+                            @else
+                                <a class="active">{{ $day->getNumber() }}</a>
+                            @endif
+                        @else
+                            <span class="disabled">{{ $day->getNumber() }}</span>
+                        @endif
+                    </td>
+                @endforeach
+            </tr>
+        @endforeach
+    </table>
+</div>
