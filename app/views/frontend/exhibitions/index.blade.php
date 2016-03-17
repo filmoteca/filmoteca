a6f163c3bb38a5a284fefd28b600b68898740f8d
Monica <monica@MacBook-Pro-de-Monica-3.local>
2016-03-17 16:50:34 -0600

    Cambios para la vista del calendario y exhibiciones.
    Falta:
    1.- Delimitar lineas de sinopsis y dar funcionalidad al botón "ver más"
    2.- Mejorar la vista, funcionalidad y botón de ver más horarios en la sección "se presenta en las salas"
    3.- Dar estilo al calendario

--- app/views/frontend/exhibitions/index.blade.php
+++ app/views/frontend/exhibitions/index.blade.php
@@ -31,27 +31,7 @@
     )
     </h1>
 
-    <div class="row">
-        <ul>
-            <div class="informacion">
-
-                <div id="accordion-resizer" class="ui-widget-content">
-
-                    <div id="accordion">
-                        <h5>Dirección General</h5>
-
-                        <div>
-                            <li>
-                                <span class="nombre">María Guadalupe Ferrer Andrade</span>
-                                <br>
-                                <span class="cargo">Directora General</span>
-                            </li>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </ul>
-    </div>
+    <br>
 
     @foreach ($exhibitions as $exhibition)
         <div class="panel panel-default">
@@ -70,96 +50,106 @@
             <div class="panel-body">
                 <div class="row">
                     <div class="col-md-4">
+                        <br><br><br>
                         <img src="{{ $exhibition->getFilm()->getCover()->getMediumImageUrl() }}">
                     </div>
                     <div class="col-md-8">
-                        <h2>{{ $exhibition->getFilm()->getTitle() }}</h2>
+                        <span style="color:#810069"><h2 align='center'>{{ $exhibition->getFilm()->getTitle() }}</h2></span>
 
                         <!-- Texto que mostrará duración, fecha y año -->
                         <h6 align='center'>
-                            <span class="countries">{{ HTML::implode($exhibition->getFilm()->getCountries(), 'name') }}</span>
+                            <span class="countries">{{ HTML::implode( $exhibition->getFilm()->getCountries(), 'name') }}</span>
+                            <span> / </span>
                             <span class="years">{{ implode(',', $exhibition->getFilm()->getYears()) }}</span>
+                            <span> / </span>
                             <span class="duration">{{ $exhibition->getFilm()->getDuration() }} min.</span>
                         </h6>
+                        
+                        <!-- Pestañas de sinopsis y trailer-->
+                            <div class="content">
+                                        <!-- Nav tabs -->
+                                <div role="tabpanel">
+                                    <ul class="nav nav-tabs" role="tablist">
+                                        <li class="active" role="presentation">
+                                            <a data-toggle="tab" role="tab" href="#tab-1">Sinopsis</a>
+                                        </li>
+                                        <li class="" role="presentation">
+                                            <a data-toggle="tab" role="tab" href="#tab-2">Trailer</a>
+                                        </li>
+                                    </ul>
 
-                        <!-- Nav tabs -->
-                        <div role="tabpanel">
-                            <ul class="nav nav-tabs" role="tablist">
-                                <li role="presentation" class="active">
-                                    <a href="#tabs-1">Sinopsis</a>
-                                </li>
-                                <li role="presentation" class="dropdown">
-                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#tabs-2">Trailer</a>
-                                </li>
-                            </ul>
+                                        <!-- Tab panes -->
+                                    <div class="tab-content">
+                                        <div id="tab-1" class="tab-pane active" role="tabpanel">
+                                            <li class="list-group-item">
+                                                <p>{{ $exhibition->getFilm()->getSynopsis() }}</p>
+                                                
+                                                <!-- Botón que desplegará más información (sinopsis completa y ficha tecnica) -->
+                                                <div align="right">
+                                                    <button  id="boton1" type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo1"
+                                                            title="Ver más">@lang('exhibitions.show.see_more')
+                                                    </button>
+                                                    <div align="left" id="demo1" class="collapse">
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                                            minim veniam</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </div>
 
-                            <!-- Tab panes -->
-                            <div class="tab-contnet">
-                                <div role="tabpanel" class="tab-pane" active id="tabs-1">
-                                <p>{{ $exhibition->getFilm()->getSynopsis() }}</p>
-                            </div>
-
-                            <!-- Botón que desplegará más información (sinopsis completa y ficha tecnica) -->
-                            <div align="right">
-                                <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo"
-                                        title="Ver más">Ver más »
-                                </button>
-                                <div id="demo" class="collapse in">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                        minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                        consequat.</p>
+                                        <!-- Video que se muestra en la pestaña Trailer(tab-2) /4by3-->
+                                        <div class="tab-pane" role="tabpanel" id="tab-2">
+                                            <li class="list-group-item embed-responsive embed-responsive-16by9">
+                                                <p>{{ $exhibition->getFilm()->getTrailer() }}</p>
+                                            </li>
+                                        </div>  
+                                    </div>
                                 </div>
                             </div>
 
-                            <!-- Video que debera ubicarse/mostrarse en la pestaña Trailer(tabs-2) -->
-                            <div role="tabpanel" class="tab-pane" id="tabs-2">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <p>{{ $exhibition->getFilm()->getTrailer() }}</p>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
 
-                </div>
-            </div>
 
-
-            <div class="panel panel-default">
-                <div class="panel-heading-int">
-                    <h3>@lang('exhibitions.show.is_presented_at')</h3>
-                </div>
-                <div class="panel-body">
-                    @foreach ($exhibition->getSchedules()->groupByAuditorium() as $scheduleGroup)
-                        <div class="row">
-                            <div class="col-md-6">
-                            <span class="auditorium-name">
-                                {{ $scheduleGroup->getAuditorium()->getName() }}
-                            </span>
-                                <a href="#">@lang('exhibitions.show.see_more')</a>
-                            </div>
-                            <div class="col-md-6">
-                                {{ HTML::schedulesTimeAsList($scheduleGroup->getSchedules()) }}
-                            </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3>@lang('exhibitions.show.is_presented_at')</h3>
                         </div>
-                        @endforeach
+                        <div class="panel-body">
+                            @foreach ($exhibition->getSchedules()->groupByAuditorium() as $scheduleGroup)
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <span class="auditorium-name">
+                                            {{ $scheduleGroup->getAuditorium()->getName() }}
+                                        </span>
+                                        <a href="#">@lang('exhibitions.show.see_ubication')</a>
+                                    </div>
+                                    <div class="col-md-6">
+                                        {{ HTML::schedulesTimeAsList($scheduleGroup->getSchedules()) }}
+                                    </div>
+                                </div>
+                            @endforeach
+                                        <!-- Botón que desplegará más horarios -->
 
-                                <!-- Botón que desplegará más horarios -->
-
-                        <div align="right">
-                            <button type="button" class="btn btn-default" data-toggle="collapse in" data-target="#demo"
-                                    title="Ver más horarios">Ver más horarios »
-                            </button>
-                            <div id="demo" class="collapse">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                    veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</p>
-                            </div>
+                                <div align="right">
+                                    <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo"
+                                                title="Ver más horarios">@lang('exhibitions.show.see_more_schedules')
+                                    </button>
+                                    <div align="left" id="demo" class="collapse">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                                minim veniam,
+                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat.</p>
+                                    </div>
+                                </div>
                         </div>
-                </div>
+                    </div>
             </div>
+        </div>
+
     @endforeach
 @stop
+
--- app/views/frontend/exhibitions/partials/calendar.blade.php
+++ app/views/frontend/exhibitions/partials/calendar.blade.php
@@ -1,50 +1,152 @@
-<div class="calendar">
-    <div class="header">
-        <h3>@lang('exhibitions.frontend.calendar.title')</h3>
-        <h4>@lang('exhibitions.frontend.calendar.subtitle')</h4>
+<!-- @section('scripts')
+    {{ HTML::script('/assets/js/facebook.js')}}
+@stop -->
+
+
+@section('sidebar')
+    
+    <span class="glyphicon glyphicon-search pull-left"></span>
+
+        <div class="search-autocomplete pull-right">
+            <angucomplete-alt class="ng-isolate-scope" input-class="form-control form-control-small" text-no-results="Ninguna exhibición encontrada" text-searching="Buscando..." minlength="1" image-field="thumbnail" search-fields="title,director" description-field="director" title-field="title" local-data="advices" selected-object="adviceSelected" pause="0" place-holder="Buscar por título o director">
+                <div class="angucomplete-holder">
+                    <input id="_value" class="form-control form-control-small" type="text" ng-change="inputChangeHandler(searchStr)" autocomplete="off" autocorrect="off" autocapitalize="off" ng-blur="hideResults()" ng-focus="resetHideResults()" placeholder="" ng-model="searchStr">
+                </div>
+            </angucomplete-alt>
+        </div>
+
+
+    <br><br><br>
+
+
+    <div class="flm-section programming">
+        <div class="content">
+
+            <div class="well-sm">
+
+                <div class="flm-section flm-subsection carrousel">
+                    <div class="calendar">
+                        <div class="header">
+                            <h3 align="center">@lang('exhibitions.frontend.calendar.title')</h3>
+                            <h4 align="center">@lang('exhibitions.frontend.calendar.subtitle')</h4>
+                        </div>
+                        <table align="center">
+                            <tr>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Su')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Mo')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Tu')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.We')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Th')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Fr')
+                                </th>
+                                <th>
+                                    @lang('dates.week-days-abbreviations.Sa')
+                                </th>
+                            </tr>
+                            @foreach ($calendar->getWeeks() as $week)
+                                <tr>
+                                    @foreach($week->getDays() as $day)
+                                        <td>
+                                            @if ($day->getDate()->month === $date->month)
+                                                @if ($day->hasExhibitions())
+                                                    <a class="active"
+                                                       title="@lang('exhibitions.frontend.calendar.exhibitions-in-the-day', ['number' => $day->getExhibitionsNumber()]) "
+                                                       href="{{ $day->getDateInFormatToUrl() }}">{{ $day->getNumber() }}</a>
+                                                @else
+                                                    <a class="active">{{ $day->getNumber() }}</a>
+                                                @endif
+                                            @else
+                                                <span class="disabled">{{ $day->getNumber() }}</span>
+                                            @endif
+                                        </td>
+                                    @endforeach
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+             <!-- Esta sección se agergara en un advertisement
+                 
+            <div class="film-subsection queries">
+                <div class="content">
+                
+                        <div class="link">
+                            <a href="http://www.filmoteca.unam.mx/exhibition">
+                                <span class="icon icon-cinemas"></span>Salas de cine
+                            </a>
+                        </div>
+                        <div class="link">
+                            <a href="http://www.filmoteca.unam.mx/exhibition">
+                                <span class="icon icon-ciclos"></span>Ciclos y funciones especiales
+                            </a>
+                        </div>
+
+                    <style>
+                        .responsiveImage {max-width: 100%; width: auto; height: auto;}
+                    </style>
+                    <div class="image pull-left img-responsive" width="600" height="800">
+                        <a href="http://www.filmoteca.unam.mx/pages/programacion/invitaciones">
+                            <img src="/imgs/home/invitaciones/descarga-invitaciones.gif" class="responsiveImage">
+                        </a>
+                    </div>
+
+                    <div class="flm-section flm-subsection digital-billboard">
+                        <div class="image pull-left">
+                            <img src="/imgs/home/cartelera/cartelera-home-marzo2016.jpg">
+                        </div>
+                            <div class="caption pull-right">
+                                <a target="_blank" alt="Consulta la cartelera digital de marzo 2016" title="Consulta la cartelera digital" href="https://issuu.com/filmotecaunam/docs/cartelera.digital.marzo.2016/1">
+                                Consulta
+                                <span class="icon icon-download pull-right"></span>
+                                </a>
+                            </div>
+                            <div class="caption pull-right">
+                                <a target="_blank" alt="Descarga la cartelera digital" title="Descarga la cartelera digital" href="/pdf/cartelera/Cartelera.Digital.Marzo.2016.pdf">
+                                Descarga
+                                <span class="icon icon-download pull-right"></span>
+                                </a>
+                            </div>
+                    </div>
+                </div>
+            </div>--> 
+            
+        </div>
     </div>
-    <table>
-        <tr>
-            <th>
-                @lang('dates.week-days-abbreviations.Su')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.Mo')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.Tu')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.We')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.Th')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.Fr')
-            </th>
-            <th>
-                @lang('dates.week-days-abbreviations.Sa')
-            </th>
-        </tr>
-        @foreach ($calendar->getWeeks() as $week)
-            <tr>
-                @foreach($week->getDays() as $day)
-                    <td>
-                        @if ($day->getDate()->month === $date->month)
-                            @if ($day->hasExhibitions())
-                                <a class="active"
-                                   title="@lang('exhibitions.frontend.calendar.exhibitions-in-the-day', ['number' => $day->getExhibitionsNumber()]) "
-                                   href="{{ $day->getDateInFormatToUrl() }}">{{ $day->getNumber() }}</a>
-                            @else
-                                <a class="active">{{ $day->getNumber() }}</a>
-                            @endif
-                        @else
-                            <span class="disabled">{{ $day->getNumber() }}</span>
-                        @endif
-                    </td>
-                @endforeach
-            </tr>
-        @endforeach
-    </table>
-</div>
+
+    <div class="subscribe-box hidden-xs">
+           <br><br><br><br><br><br><br>      
+        <p>Recibe nuestra cartelera digital</p>
+
+         <div class="input-group input-group-sm">
+            <input type="email" name="email" placeholder="Ingresa tu correo electrónico" class="form-control">
+            <span class="input-group-addon">@</span>
+        </div>
+
+        <button type="button" class="btn btn-success">Enviar</button>
+    </div>
+
+    <br><br>
+            
+    <div class="fb-page" data-href="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+        <div class="fb-xfbml-parse-ignore">
+            <blockquote cite="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts">
+                <a href="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts">Comunidad  Cines UNAM</a>
+            </blockquote>
+        </div>
+    </div>
+
+
+@stop
