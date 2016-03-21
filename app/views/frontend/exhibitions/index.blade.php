3fa07bc4a51f36c20337267279ad38ffc46329bc
Victor Aguilar <victor.aguilar@clickbus.com>
2016-03-21 16:16:10 -0600

    Code style

--- app/views/frontend/exhibitions/index.blade.php
+++ app/views/frontend/exhibitions/index.blade.php
@@ -54,7 +54,8 @@
                         <img src="{{ $exhibition->getFilm()->getCover()->getMediumImageUrl() }}">
                     </div>
                     <div class="col-md-8">
-                        <span style="color:#810069"><h2 align='center'>{{ $exhibition->getFilm()->getTitle() }}</h2></span>
+                        <span style="color:#810069"><h2
+                                    align='center'>{{ $exhibition->getFilm()->getTitle() }}</h2></span>
 
                         <!-- Texto que mostrará duración, fecha y año -->
                         <h6 align='center'>
@@ -63,89 +64,93 @@
                             <span> / </span>
                             <span class="duration">{{ $exhibition->getFilm()->getDuration() }} min.</span>
                         </h6>
-                        
+
                         <!-- Pestañas de sinopsis y trailer-->
-                            <div class="content">
-                                        <!-- Nav tabs -->
-                                <div role="tabpanel">
-                                    <ul class="nav nav-tabs" role="tablist">
-                                        <li class="active" role="presentation">
-                                            <a data-toggle="tab" role="tab" href="#tab-1">Sinopsis</a>
-                                        </li>
-                                        <li class="" role="presentation">
-                                            <a data-toggle="tab" role="tab" href="#tab-2">Trailer</a>
-                                        </li>
-                                    </ul>
+                        <div class="content">
+                            <!-- Nav tabs -->
+                            <div role="tabpanel">
+                                <ul class="nav nav-tabs" role="tablist">
+                                    <li class="active" role="presentation">
+                                        <a data-toggle="tab" role="tab" href="#tab-1">Sinopsis</a>
+                                    </li>
+                                    <li class="" role="presentation">
+                                        <a data-toggle="tab" role="tab" href="#tab-2">Trailer</a>
+                                    </li>
+                                </ul>
 
-                                        <!-- Tab panes -->
-                                    <div class="tab-content">
-                                        <div id="tab-1" class="tab-pane active" role="tabpanel">
-                                            <li class="list-group-item">
-                                                <p>{{ $exhibition->getFilm()->getSynopsis() }}</p>
-                                                
-                                                <!-- Botón que desplegará más información (sinopsis completa y ficha tecnica) -->
-                                                <div align="right">
-                                                    <button  id="boton1" type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo1"
-                                                            title="Ver más">@lang('exhibitions.show.see_more')
-                                                    </button>
-                                                    <div align="left" id="demo1" class="collapse">
-                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                            minim veniam</p>
-                                                    </div>
+                                <!-- Tab panes -->
+                                <div class="tab-content">
+                                    <div id="tab-1" class="tab-pane active" role="tabpanel">
+                                        <li class="list-group-item">
+                                            <p>{{ $exhibition->getFilm()->getSynopsis() }}</p>
+
+                                            <!-- Botón que desplegará más información (sinopsis completa y ficha tecnica) -->
+                                            <div align="right">
+                                                <button id="boton1" type="button" class="btn btn-default"
+                                                        data-toggle="collapse" data-target="#demo1"
+                                                        title="Ver más">@lang('exhibitions.show.see_more')
+                                                </button>
+                                                <div align="left" id="demo1" class="collapse">
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                                                        aliqua. Ut enim ad
+                                                        minim veniam</p>
                                                 </div>
-                                            </li>
-                                        </div>
+                                            </div>
+                                        </li>
+                                    </div>
 
-                                        <!-- Video que se muestra en la pestaña Trailer(tab-2) /4by3-->
-                                        <div class="tab-pane" role="tabpanel" id="tab-2">
-                                            <li class="list-group-item embed-responsive embed-responsive-16by9">
-                                                <p>{{ $exhibition->getFilm()->getTrailer() }}</p>
-                                            </li>
-                                        </div>  
+                                    <!-- Video que se muestra en la pestaña Trailer(tab-2) /4by3-->
+                                    <div class="tab-pane" role="tabpanel" id="tab-2">
+                                        <li class="list-group-item embed-responsive embed-responsive-16by9">
+                                            <p>{{ $exhibition->getFilm()->getTrailer() }}</p>
+                                        </li>
                                     </div>
                                 </div>
                             </div>
-
-                            
                         </div>
+
+
                     </div>
+                </div>
 
 
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3>@lang('exhibitions.show.is_presented_at')</h3>
-                        </div>
-                        <div class="panel-body">
-                            @foreach ($exhibition->getSchedules()->groupByAuditorium() as $scheduleGroup)
-                                <div class="row">
-                                    <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3>@lang('exhibitions.show.is_presented_at')</h3>
+                    </div>
+                    <div class="panel-body">
+                        @foreach ($exhibition->getSchedules()->groupByAuditorium() as $scheduleGroup)
+                            <div class="row">
+                                <div class="col-md-6">
                                         <span class="auditorium-name">
                                             {{ $scheduleGroup->getAuditorium()->getName() }}
                                         </span>
-                                        <a href="#">@lang('exhibitions.show.see_ubication')</a>
-                                    </div>
-                                    <div class="col-md-6">
-                                        {{ HTML::schedulesTimeAsList($scheduleGroup->getSchedules()) }}
-                                    </div>
+                                    <a href="#">@lang('exhibitions.show.see_ubication')</a>
                                 </div>
+                                <div class="col-md-6">
+                                    {{ HTML::schedulesTimeAsList($scheduleGroup->getSchedules()) }}
+                                </div>
+                            </div>
                             @endforeach
-                                        <!-- Botón que desplegará más horarios -->
+                                    <!-- Botón que desplegará más horarios -->
 
-                                <div align="right">
-                                    <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo"
-                                                title="Ver más horarios">@lang('exhibitions.show.see_more_schedules')
-                                    </button>
-                                    <div align="left" id="demo" class="collapse">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                minim veniam,
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</p>
-                                    </div>
+                            <div align="right">
+                                <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#demo"
+                                        title="Ver más horarios">@lang('exhibitions.show.see_more_schedules')
+                                </button>
+                                <div align="left" id="demo" class="collapse">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat.
+                                    </p>
                                 </div>
-                        </div>
+                            </div>
                     </div>
+                </div>
             </div>
         </div>
 
