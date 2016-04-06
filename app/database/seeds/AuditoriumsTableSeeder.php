6617bb35c98b81213b39696427bfeb69f41770a5
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-04-05 23:38:17 -0500

    Updated seeder of auditoriums

--- app/database/seeds/AuditoriumsTableSeeder.php
+++ app/database/seeds/AuditoriumsTableSeeder.php
@@ -1,39 +1,41 @@
 <?php
 
-class AuditoriumsTableSeeder extends Seeder {
+class AuditoriumsTableSeeder extends Seeder
+{
+    const NUMBER = 6;
 
     public function run()
     {
-    	$auditoriums = array();
+        $auditoriums = [];
+        $faker = Faker\Factory::create('es_MX');
 
-    	$names = App::make('names');
+        for ($i = 0; $i < self::NUMBER; $i++) {
+            $name = $faker->name;
+            $price = $faker->randomNumber(2);
 
-    	$namesLen = count($names) - 1;
-
-        for( $i = 0; $i < 6; $i++)
-        {
-            $auditorium = array(
-                'name' => $names[rand(0,$namesLen)] . ' ' . $names[rand(0,$namesLen)],
-                'address' => 'Una dirección haci aun lugar muy muy lejano.',
-                'telephone' => '21579919, 20407979',
+            $auditorium = [
+                'name' => $name,
+                'slug' => \Illuminate\Support\Str::slug($name),
+                'address' => $faker->address,
+                'telephone' => implode(',', [$faker->phoneNumber, $faker->phoneNumber]),
                 'schedule' => 'De 9 de la noche a 10 de la mañana.',
-                'general_cost' => rand(0,500) / 10,
-                'special_cost' => rand(0, 500) / 10,
+                'general_cost' => $price,
+                'special_cost' => $price + 50,
                 'map' => 'https://www.google.com/maps/@19.3304256,-99.274622,14z',
                 'created_at' => new DateTime(),
                 'updated_at' => new DateTime()
-                );
+            ];
 
-            if( rand(0,1) == 1 && $i > 1)
-            {
+            if (rand(0, 1) == 1 && $i > 1) {
                 array_add($auditoriums, 'venue', 1);
             }
 
             array_push($auditoriums, $auditorium);
         }
 
-        DB::table('auditoriums')->truncate();
 
+
+        DB::table('auditoriums')->truncate();
         DB::table('auditoriums')->insert($auditoriums);
     }
 
