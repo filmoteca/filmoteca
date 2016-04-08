2d055b833d8917f6d9ebb5fb890a52520258cf33
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-04-07 22:27:57 -0500

    Added link to history

--- app/views/exhibitions/frontend/exhibitions/partials/programming.blade.php
+++ app/views/exhibitions/frontend/exhibitions/partials/programming.blade.php
@@ -6,5 +6,8 @@
 		<a href="{{ URL::route('exhibition.auditorium.index') }}">
 			@lang('exhibitions.frontend.auditorium.index.title')
 		</a>
+		<a href="{{ URL::route('exhibition.history') }}">
+			@lang('exhibitions.frontend.history.title')
+		</a>
 	</div>
 </div>
