4d5268a4d6a65dbf890c5ab0683eddfa7aecbdde
monyc2000 <monica.guzman.r@gmail.com>
2016-04-29 21:18:52 -0500

    Avances 29Abril2016: Estilos de facebook sidebar

--- app/views/exhibitions/layouts/frontend.blade.php
+++ app/views/exhibitions/layouts/frontend.blade.php
@@ -29,13 +29,13 @@
 
     <br><br>
 
-    <div class="fb-page"
-         data-href="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts"
-         data-tabs="timeline"
-         data-small-header="true"
-         data-adapt-container-width="true"
-         data-hide-cover="false"
-         data-show-facepile="true">
+    <div class="fb-page" 
+        data-href="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts" 
+        data-tabs="timeline" 
+        data-small-header="true" 
+        data-adapt-container-width="true" 
+        data-hide-cover="false" 
+        data-show-facepile="true">
         <div class="fb-xfbml-parse-ignore">
             <blockquote cite="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts">
                 <a href="https://www.facebook.com/Comunidad.Cines.UNAM/?fref=ts">
