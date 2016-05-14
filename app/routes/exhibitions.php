e844ea075955c7d29a8ae9013fa3d888bf66e9ab
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-05-14 14:04:05 -0500

    Translated exhibitions routes to spanish

--- app/routes/exhibitions.php
+++ app/routes/exhibitions.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'exhibition'], function () {
+Route::group(['prefix' => 'programacion'], function () {
 
     Route::get('/', [
         'as' => 'exhibitions.frontend.exhibitions.index',
@@ -13,14 +13,9 @@ Route::group(['prefix' => 'exhibition'], function () {
         // Match example: 2-marzo-2015, 23-febrero-1998
     ])->where('date', '[0-9]{1,2}-[a-z]+-[0-9]{4}');
 
-    Route::get('history', [
+    Route::get('historico-de-programacion', [
         'as' => 'exhibition.history',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\ExhibitionController@history'
-    ]);
-
-    Route::get('films-searcher', [
-        'as' => 'exhibitions.frontend.exhibitions.filmssearcher',
-        'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\ExhibitionController@searchFilms'
     ]);
 
     Route::get('{id}/show', [
@@ -28,43 +23,50 @@ Route::group(['prefix' => 'exhibition'], function () {
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\ExhibitionController@show'
     ]);
 
-    Route::get('{id}/schedule', [
+    Route::get('{id}/programacion', [
         'as' => 'exhibition.schedule.search',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\ScheduleController@search'
     ]);
 
-    Route::get('auditorium', [
+    Route::get('salas', [
         'as' => 'exhibition.auditorium.index',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\AuditoriumController@index'
     ]);
 
-    Route::get('auditorium/{slug}', [
+    Route::get('sala/{slug}', [
         'as' => 'exhibition.auditorium.show',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\AuditoriumController@show'
     ]);
 
-    Route::get('cycle', [
+    Route::get('ciclos', [
         'as' => 'exhibitions.frontend.cycle.index',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\CycleController@index'
     ]);
 
-    Route::get('cycle/{slug}', [
+    Route::get('ciclo/{slug}', [
         'as' => 'exhibitions.frontend.cycle.show',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\CycleController@show'
     ]);
 
-    Route::get('film/{slug}', [
+    Route::get('pelicula/{slug}', [
         'as' => 'exhibitions.frontend.film.show',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\FilmController@show'
     ]);
+
+    Route::get('cartelera', [
+        'as' => 'exhibitions.frontend.billboard.index',
+        'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\BillboardController@index'
+    ]);
+
+    // Urls used by the system and do not used for SEO. Then the translation is unnecessary
 
     Route::get('billboard/subscribe', [
         'as' => 'exhibitions.frontend.billboard.subscribe',
         'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\BillboardController@subscribe'
     ]);
 
-    Route::get('billboard', [
-        'as' => 'exhibitions.frontend.billboard.index',
-        'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\BillboardController@index'
+    Route::get('films-searcher', [
+        'as' => 'exhibitions.frontend.exhibitions.filmssearcher',
+        'uses' => 'Filmoteca\Exhibitions\Controllers\Frontend\ExhibitionController@searchFilms'
     ]);
 });
