2a2d88ce609c7b280579a93e8971d278cc90bb1c
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-05-15 21:16:52 -0500

    Fixed routes in main menu

--- app/views/elements/menus/main.blade.php
+++ app/views/elements/menus/main.blade.php
@@ -21,20 +21,20 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/quienes-somos/mision-y-vision">Misión y Visión</a></li>
-                 
+
                   <li><a href="/pages/quienes-somos/objetivos-y-funciones">Objetivos y Funciones</a></li>
-                
+
                   <li><a href="/pages/quienes-somos/consejo-asesor">Consejo Asesor</a></li>
-                  
+
                   <li><a href="/pages/quienes-somos/memoria-filmoteca">Memoria Filmoteca</a></li>
-                  
+
                   <li><a href="/pages/quienes-somos/cronologia">Cronología</a></li>
-               
+
                   <li><a href="/pages/quienes-somos/medalla-filmoteca">Medalla Filmoteca</a>
                   </li>
-                  
+
                   <li><a href="/pages/quienes-somos/libro-filmoteca50">Libro Filmoteca: 50 años</a></li>
-                 
+
                   <li><a href="/pages/quienes-somos/directorio">Directorio</a></li>
 
                   <li><a href="/pages/aniversario55/index">55 Aniversario</a></li>
@@ -47,15 +47,15 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/acervo/filmico">Fílmico</a></li>
-                  
+
                   <li><a href="/pages/acervo/aparatos-antiguos">Aparatos antiguos</a></li>
-                  
+
                   <li><a href="/pages/acervo/biblioteca">Biblioteca</a></li>
-                  
+
                   <li><a href="/pages/acervo/restauracion">Restauración</a></li>
-                  
+
                   <li ><a href="/pages/acervo/museo-virtual">Museo virtual</a></li>
-                  
+
                   <li><a href="/pages/acervo/cine-en-linea">Cine en línea</a></li>
                 </ul>
               </li>
@@ -66,23 +66,23 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/servicios/banco-de-imagen">Banco de imagen</a></li>
-                  
+
                   <li><a href="/pages/servicios/departamento-de-catalogacion">Catalogación</a></li>
-                  
+
                   <li><a href="/pages/servicios/centro-de-documentacion">Centro de documentación</a></li>
-                  
+
                   <li ><a href="/pages/servicios/deposito-y-resguardo">Depósito y resguardo</a></li>
-                  
+
                   <li><a href="/pages/servicios/laboratorio-cinematografico">Laboratorio cinematográfico</a></li>
 
                   <li><a href="/pages/servicios/laboratorio-digital">Laboratorio digital</a></li>
-                  
+
                   <li><a href="/pages/servicios/prestamo-y-alquiler-de-peliculas">Préstamo y alquiler de películas</a></li>
-                  
+
                   <li><a href="/pages/servicios/departamento-de-produccion">Producción</a></li>
-                  
+
                   <li><a href="/pages/servicios/taller-de-restauracion">Taller de restauración</a></li>
-                  
+
                   <li><a href="/pages/servicios/requisitos-para-solicitar-un-servicio">Requisitos para solicitar un servicio</a></li>
                 </ul>
               </li>
@@ -92,13 +92,13 @@
                   PROGRAMACIÓN<b class="caret"></b>
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a href="{{ URL::to('exhibition') }}">Programación del mes</a></li>
-                  
-                  <li><a href="/billboard">Cartelera digital</a></li>
-                  
+                  <li><a href="{{ URL::route('exhibitions.frontend.exhibitions.index') }}">Programación del mes</a></li>
+
+                  <li><a href="{{ URL::route('exhibitions.frontend.billboard.index') }}">Cartelera digital</a></li>
+
                   <li><a href="/pages/programacion/invitaciones">Invitaciones</a></li>
-                  
-                  <li ><a href="{{ URL::to('exhibition/history')}}">Histórico</a></li>
+
+                  <li ><a href="{{ URL::route('exhibition.history')}}">Histórico</a></li>
                 </ul>
               </li>
 
@@ -108,15 +108,15 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/press_register">Prensa</a></li>
-                  
+
                   <li><a href="/pages/difusion/exposiciones-museografia">Exposiciones</a></li>
-                  
+
                   <li><a href="/pages/difusion/publicaciones">Publicaciones</a></li>
-                  
+
                   <li><a href="/news/index">Noticias</a></li>
-                  
+
                   <li ><a href="/pages/difusion/filmoteca-in-the-media">Filmoteca en los medios</a></li>
-                  
+
                   <li><a href="/pages/difusion/interviews">La Filmoteca recomienda</a></li>
                 </ul>
               </li>
@@ -127,11 +127,11 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/cursos/cursos-y-talleres">Cursos y talleres</a></li>
-                  
+
                   <li><a href="/pages/extension-academica/servicio-social">Servicio social</a></li>
-                  
+
                   <li><a href="/pages/extension-academica/becarios">Becarios</a></li>
-                  
+
                   <li><a href="/pages/extension-academica/visitas-guiadas">Visitas guiadas</a></li>
                 </ul>
               </li>
@@ -142,11 +142,11 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/concursos/jose-rovirosa">José Rovirosa</a></li>
-                  
+
                   <li><a href="/pages/concursos/alfonso-reyes">Alfonso Reyes "Fósforo"</a></li>
-                  
+
                   <li><a href="/pages/concursos/corto-movil">Corto Móvil</a></li>
-                  
+
                   <li ><a href="/pages/concursos/convocatorias">Convocatorias</a></li>
                 </ul>
               </li>
@@ -160,9 +160,9 @@
                   <li><a href="/pages/libreria/proximamente-venta-linea">Tienda en línea</a></li>
 
                   <!-- <li><a href="http://cine.libros.unam.mx/">Venta en línea</a></li>-->
-                  
+
                  <li><a href="/pages/libreria/catalogo">Catálogo</a></li>
-                  
+
                 </ul>
               </li>
 
