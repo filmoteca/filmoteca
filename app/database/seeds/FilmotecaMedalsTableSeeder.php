79c0e781044fc6ec6fc1974a3d035607126ffa51
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2016-08-28 12:20:32 -0500

    Created seeder to filmoteca medals

---
+++ app/database/seeds/FilmotecaMedalsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+class FilmotecaMedalsTableSeeder extends Seeder
+{
+    const AMOUNT = 50;
+
+    public function run()
+    {
+        $faker = Faker\Factory::create('es_MX');
+        $types = [];
+
+        for ($i = 0; $i < self::AMOUNT; $i++) {
+            $name = $faker->sentence(rand(2, 6));
+            $currentDate = new DateTime();
+
+            $type = [
+                'name' => $name,
+                'biography' => $faker->text(600),
+                'award_date' => rand(1850, intval($currentDate->format('Y'))),
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ];
+
+            $types[] = $type;
+        }
+
+        DB::table('filmoteca_medals')->truncate();
+        DB::table('filmoteca_medals')->insert($types);
+    }
+}
