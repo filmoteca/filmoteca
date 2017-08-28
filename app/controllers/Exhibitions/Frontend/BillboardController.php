b9720095bba299c01864e67931ba001e64552253
Monica <monica@MacBook-Pro-de-Monica-3.local>
2017-08-28 17:04:21 -0500

    Archivos para agregar paquete y servicio de subscripciónn a programación con mailchimp

--- app/controllers/Exhibitions/Frontend/BillboardController.php
+++ app/controllers/Exhibitions/Frontend/BillboardController.php
@@ -5,6 +5,7 @@ namespace Filmoteca\Exhibitions\Controllers\Frontend;
 use Filmoteca\Repository\BillboardsRepository;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
 
 /**
  * Class BillboardController
@@ -55,6 +56,65 @@ class BillboardController extends Controller
 
     public function subscribe()
     {
+            session_start();
+            if(isset($_POST['submit'])){
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+                    // MailChimp API credentials
+                    $apiKey = 'e662d3bd1a71e42f2d942e36afe8df74-us16';
+                    $listID = '7b682a5a51';
+                    
+                    // MailChimp API URL
+                    $memberID = md5(strtolower($email));
+                    $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+                    $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
+                    
+                    // member information
+                    $json = json_encode([
+                        'email_address' => $email,
+                        'status'        => 'subscribed',
+                        'merge_fields'  => [
+                            'FNAME'     => $fname,
+                            'LNAME'     => $lname
+                        ]
+                    ]);
+                    
+                    // send a HTTP POST request with curl
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+                    $result = curl_exec($ch);
+                    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                    curl_close($ch);
+                    
+                    // store the status message based on response code
+                    if ($httpCode == 200) {
+                        $_SESSION['msg'] = '<p style="color: #34A853">Tu subscripción a la cartelera está lista.</p>';
+                    } else {
+                        switch ($httpCode) {
+                            case 214:
+                                $msg = 'Ya estás subscrito.';
+                                break;
+                            default:
+                                $msg = 'Ocurrio un problema, intentalo de nuevo.';
+                                break;
+                        }
+                        $_SESSION['msg'] = '<p style="color: #EA4335">'.$msg.'</p>';
+                    }
+                }else{
+                    $_SESSION['msg'] = '<p style="color: #EA4335">Por favor ingresa un correo electrónico valido.</p>';
+                }
+            }
+            // redirect to homepage
+            
+         return \Redirect::to(\Input::get('fromchimp'));
 
     }
 }
--- app/views/exhibitions/frontend/exhibitions/partials/billboard-subscription-form.blade.php
+++ app/views/exhibitions/frontend/exhibitions/partials/billboard-subscription-form.blade.php
@@ -1,12 +1,30 @@
+<?php session_start(); // place it on the top of the script ?>
+<?php
+    $statusMsg = !empty($_SESSION['msg'])?$_SESSION['msg']:'';
+    unset($_SESSION['msg']);
+    echo $statusMsg;
+?>
+
 <div class="subscribe-box hidden-xs">
-    {{ Form::open(['route' => 'exhibitions.frontend.billboard.subscribe', 'method' => 'GET']) }}
-    <p>@lang('exhibitions.frontend.exhibition.show.receive_our_digital_billboard')</p>
+   {{ Form::open(array('route' => 'exhibitions.frontend.billboard.subscribe')) }}
+	    <p>@lang('exhibitions.frontend.exhibition.show.receive_our_digital_billboard')</p>
 
-    <div class="input-group input-group-sm">
-        <input type="email" name="email" placeholder="@lang('exhibitions.frontend.exhibition.show.enter_your_email')" class="form-control">
-        <span class="input-group-addon">@</span>
-    </div>
+		{{ Form::hidden('fromchimp', Request::path()) }}
 
-    <button type="button" class="btn btn-success">@lang('exhibitions.frontend.exhibition.show.send')</button>
+			@if (Session::has('msg'))
+				<div class="alert alert-success">
+					{{ Session::get('msg') }}
+				</div>
+			@endif
+
+	    <div class="input-group input-group-sm">
+	        <input type="text" name="fname" placeholder="@lang('exhibitions.frontend.exhibition.show.enter_your_name')" class="form-control">
+	        <input type="text" name="lname" placeholder="@lang('exhibitions.frontend.exhibition.show.enter_your_last')" class="form-control">
+	        <input type="email" name="email" placeholder="@lang('exhibitions.frontend.exhibition.show.enter_your_email')" class="form-control">
+	        <span class="input-group-addon">@</span>
+	    </div>
+
+	    <button type="submit" name="submit" class="btn btn-success">@lang('exhibitions.frontend.exhibition.show.send')</button>
+    
     {{ Form::close() }}
 </div>
